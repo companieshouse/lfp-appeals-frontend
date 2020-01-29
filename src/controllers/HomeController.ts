ba9b2f4ba68750c766d13aba9110aa554461f18c
akaJSaunders <jsaunders1@companieshouse.gov.uk>
2020-01-29 15:39:25 +0000

    clean-up: Remove uneccesary comments and rename landing-page.njk to landing.njk

--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -2,5 +2,5 @@ import { Request, Response, NextFunction } from 'express';
 
 export class HomeController {
 
-    public sayHello = (req: Request, res: Response, next: NextFunction) => res.render('landing-page');
+    public sayHello = (req: Request, res: Response, next: NextFunction) => res.render('landing');
 }
