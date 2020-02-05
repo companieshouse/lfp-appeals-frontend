baa2891657ffd7427ec31b47a61e564c70693b90
Damian Dunajski <ddunajski@companieshouse.gov.uk>
2020-02-05 23:35:18 +0000

    Add integration test for health check endpoint

---
+++ test/SubstituteFactory.ts
@@ -0,0 +1,7 @@
+import { Substitute, SubstituteOf } from '@fluffy-spoon/substitute';
+
+export const createSubstituteOf = <T>(configureSubstitute: (substitute: SubstituteOf<T>) => void): T => {
+    const substitute = Substitute.for<T>();
+    configureSubstitute(substitute);
+    return substitute;
+};
