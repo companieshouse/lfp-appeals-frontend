7e326083f4987e52666add9caa210cdedae2e09e
akaJSaunders <jsaunders1@companieshouse.gov.uk>
2020-02-07 11:32:28 +0000

    feat: Setup some validation with Hapi Joi

--- src/controllers/PenaltyDetailsController.ts
+++ src/controllers/PenaltyDetailsController.ts
@@ -4,34 +4,44 @@ import { inject } from 'inversify';
 import { SessionService } from '../services/SessionService';
 import { CREATED, BAD_REQUEST, OK } from 'http-status-codes';
 import {check, validationResult, body} from 'express-validator';
+const Joi = require('@hapi/joi');
 
+const schema = Joi.object({
+    companyNumber: Joi.string()
+        .replace(' ', '')
+        .insensitive()
+        .uppercase()
+        .min(1).max(8)
+        .pattern(new RegExp('^([a-zA-Z]{2})?[0-9]{6,8}'))
+        .messages({
+            'string.empty': 'You must enter a company number',
+            'string.min': 'You must enter your full eight character company number',
+            'string.max': 'You must enter your full eight character company number',
+            'string.pattern.base': 'You must enter your full eight character company number'
+        }),
 
-const preValidation = [
-    body('companyNumber').blacklist(' ').escape().not().isEmpty().withMessage('You must enter a company number'),
-    body('companyNumber').blacklist(' ').escape().isLength({max: 8}).withMessage('You must enter your full eight character company number'),
-    body('penaltyReference').blacklist(' ').escape().not().isEmpty().withMessage('You must enter a penalty reference number'),
-    body('penaltyReference').blacklist(' ').escape().isLength({min: 9, max: 9}).withMessage('You must enter your reference number exactly as shown on your penalty notice'),
-];
+    penaltyReference: Joi.string()
+        .replace(' ', '')
+        .min(9).max(9)
+        .messages({
+            'string.empty': 'You must enter a penalty reference number',
+            'string.min': 'You must enter your reference number exactly as shown on your penalty notice',
+            'string.max': 'You must enter your reference number exactly as shown on your penalty notice'
+        })
+})
 
-const padCompanyNumber = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    let companyNumber: string = req.body.companyNumber;
-    console.log("Pad: ", companyNumber)
+const padNumber = (companyNumber: string):string => {
     if (/^([a-zA-Z]{2}?)/gm.test(companyNumber)) {
-      const leadingLetters = companyNumber.substring(0, 2);
-      let trailingChars = companyNumber.substring(2, companyNumber.length);
-      trailingChars = trailingChars.padStart(6, '0');
-      companyNumber = leadingLetters + trailingChars;
+        const leadingLetters = companyNumber.substring(0, 2);
+        let trailingChars = companyNumber.substring(2, companyNumber.length);
+        trailingChars = trailingChars.padStart(6, '0');
+        companyNumber = leadingLetters + trailingChars;
     } else {
-      companyNumber = companyNumber.padStart(8, '0');
+        companyNumber = companyNumber.padStart(8, '0');
     }
-    console.log("Padded: ", companyNumber)
-    req.body.companyNumber = companyNumber;
-    return next();
-  };
+    return companyNumber;
+};
 
-const postValidator = [
-    body('companyNumber').blacklist(' ').escape().matches('^([a-zA-Z]{2})?[0-9]{6,8}').withMessage('You must enter your full eight character company number  TOOT')
-]
 
 @controller('/penalty-reference')
 export class PenaltyDetailsController extends BaseHttpController {
@@ -58,12 +68,20 @@ export class PenaltyDetailsController extends BaseHttpController {
         .render('penaltydetails');
     }
 
-    @httpPost('', ...preValidation, padCompanyNumber, ...postValidator)
+    @httpPost('')
     public createPenaltyDetails(@request() req: Request, @response() res: Response): void {
-        const errors = validationResult(req)
-        if(!errors.isEmpty()){
-            console.log('non empty')
-            console.log(errors)
-        }
+
+        const results = schema
+            .validate(
+                {
+                    companyNumber: padNumber(req.body.companyNumber),
+                    penaltyReference: req.body.penaltyReference
+                },
+                {
+                    abortEarly: false
+                }
+            )
+        console.log(results)
+
     }
 }
