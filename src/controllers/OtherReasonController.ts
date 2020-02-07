76a026c5726d441ad962e16ccbf8503331bc4614
Damian Dunajski <damdun@gmail.com>
2020-02-07 01:04:13 +0100

    Implement simple validation logic and rendering of errors on the screen

--- src/controllers/OtherReasonController.ts
+++ src/controllers/OtherReasonController.ts
@@ -1,5 +1,21 @@
 import { BaseHttpController, controller, httpGet, httpPost } from 'inversify-express-utils';
 
+class ValidationError {
+  constructor (public readonly field: string, public readonly text: string) {}
+
+  get href(): string {
+    return `#${this.field}-error`;
+  }
+}
+
+class ValidationResult {
+  constructor (public readonly errors: ValidationError[] = []) {}
+
+  public getErrorForField(field: string): ValidationError | undefined {
+    return this.errors.find(error => error.field === field);
+  }
+}
+
 @controller('/other-reason')
 export class OtherReasonController extends BaseHttpController {
 
@@ -10,8 +26,22 @@ export class OtherReasonController extends BaseHttpController {
 
   @httpPost('')
   public handleFormSubmission(): void {
-    const body: OtherReason = this.httpContext.request.body
-    this.httpContext.response.render('other-reason', body);
+    const body: OtherReason = this.httpContext.request.body;
+
+    const validationResult = this.validate(body);
+
+    this.httpContext.response.render('other-reason', { ...body, validationResult });
   }
 
+  private validate(data: OtherReason): ValidationResult {
+    const result: ValidationResult = new ValidationResult();
+    if (!data.reason) {
+      result.errors.push(new ValidationError('reason', 'You must give your reason a title'));
+    }
+    if (!data.description) {
+      result.errors.push(new ValidationError('description', 'You must give us more information'));
+    }
+    return result;
+  }
 }
+
