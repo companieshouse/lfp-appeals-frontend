59785efddb661cc8790a0d36819ac7181b0fda93
akaJSaunders <jsaunders1@companieshouse.gov.uk>
2020-02-10 17:42:41 +0000

    Test: Add more unit tests and clean up Schemas for validation

--- src/utils/Schemas.ts
+++ src/utils/Schemas.ts
@@ -8,8 +8,6 @@ export const penaltyDetailsSchema = Joi.object({
         .regex(/^(((SC|NI)[0-9]{1,6})|([0-9]{1,8}))$/)
         .messages({
             'string.empty': 'You must enter a company number',
-            // 'string.min': 'You must enter your full eight character company number',
-            // 'string.max': 'You must enter your full eight character company number',
             'string.pattern.base': 'You must enter your full eight character company number'
         }),
     penaltyReference: Joi.string()
@@ -19,8 +17,6 @@ export const penaltyDetailsSchema = Joi.object({
         .regex(/([A-Z]{1}[0-9]{8})/)
         .messages({
             'string.empty': 'You must enter a penalty reference number',
-            // 'string.min': 'You must enter your reference number exactly as shown on your penalty notice',
-            // 'string.max': 'You must enter your reference number exactly as shown on your penalty notice',
             'string.pattern.base': 'You must enter your reference number exactly as shown on your penalty notice'
         })
 });
--- test/utils/Validate.test.ts
+++ test/utils/Validate.test.ts
@@ -5,20 +5,32 @@ import { Validate } from '../../src/utils/Validate';
 
 
 describe('Validate', () => {
-    it('should return no errors to correct input', () => {
+    it('should return no errors to two correct inputs', () => {
 
         const penaltyDetails: PenaltyReferenceDetails = {
             penaltyReference: 'A12345678',
             companyNumber: 'SC123123'
         };
         const result = Validate.validate(penaltyDetails)
-        const expectedResult = {
-            errors: []
-        }
+        const expectedResult = {errors: []}
         expect(result).to.deep.equal(expectedResult)
     });
 
-    it('should return penaltyReference errors to incorrect penaltyReference', () => {
+    it('should return two errors for two empty fields', () => {
+
+        const penaltyDetails: PenaltyReferenceDetails = {
+            penaltyReference: '',
+            companyNumber: ''
+        };
+        const result = Validate.validate(penaltyDetails)
+        const expectedResult = {
+            errors:[{field: 'companyNumber',text : 'You must enter a company number'},
+            {field: 'penaltyReference',text: 'You must enter a penalty reference number'}]
+    }
+        expect(result).to.deep.equal(expectedResult)
+    });
+
+    it('should return penaltyReference error to incorrect penalty reference number', () => {
 
         const penaltyDetails: PenaltyReferenceDetails = {
             penaltyReference: '12345678',
@@ -26,15 +38,26 @@ describe('Validate', () => {
         };
         const result = Validate.validate(penaltyDetails)
         const expectedResult = {
-                errors:[{
-                    field: 'penaltyReference',
-                    text: 'You must enter your reference number exactly as shown on your penalty notice'
-                }]
+                errors:[{field: 'penaltyReference', text: 'You must enter your reference number exactly as shown on your penalty notice'}]
         }
         expect(result).to.deep.equal(expectedResult)
     });
 
-    it('should return errors to incorrect input', () => {
+    it('should return companyNumber error to incorrect company number', () => {
+
+        const penaltyDetails: PenaltyReferenceDetails = {
+            penaltyReference: 'H12345678',
+            companyNumber: 'S1231231'
+        };
+        const result = Validate.validate(penaltyDetails)
+        const expectedResult = {
+                errors:[{field: 'companyNumber', text : 'You must enter your full eight character company number'}]
+        }
+        expect(result).to.deep.equal(expectedResult)
+    });
+
+
+    it('should return errors for both fields when both are incorrect inputs', () => {
 
         const penaltyDetails: PenaltyReferenceDetails = {
             penaltyReference: '12345678',
@@ -43,14 +66,8 @@ describe('Validate', () => {
         const result = Validate.validate(penaltyDetails)
         const expectedResult = {
                 errors:[
-                {
-                    field: 'companyNumber',
-                    text : 'You must enter your full eight character company number'
-                },
-                {
-                    field: 'penaltyReference',
-                    text: 'You must enter your reference number exactly as shown on your penalty notice'
-                }]
+                {field: 'companyNumber', text : 'You must enter your full eight character company number'},
+                {field: 'penaltyReference', text: 'You must enter your reference number exactly as shown on your penalty notice'}]
         }
         expect(result).to.deep.equal(expectedResult)
     });
