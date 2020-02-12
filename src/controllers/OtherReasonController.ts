701425683f188419a95513e72f77594444df9eb1
Damian Dunajski <ddunajski@companieshouse.gov.uk>
2020-02-12 23:45:44 +0000

    Persist form data in session

--- src/controllers/OtherReasonController.ts
+++ src/controllers/OtherReasonController.ts
@@ -1,24 +1,48 @@
+import { inject } from 'inversify';
 import { BaseHttpController, controller, httpGet, httpPost } from 'inversify-express-utils';
 import { OTHER_REASON_PAGE } from '../utils/Paths';
 import { SchemaValidator } from '../utils/validation/SchemaValidator';
 import { ValidationResult } from '../utils/validation/ValidationResult';
 import { OtherReason } from '../models/OtherReason';
 import { schema } from '../models/OtherReason.schema';
+import { RedisService } from '../services/RedisService';
+
+const sessionKey = 'session::other-reason';
 
 @controller(OTHER_REASON_PAGE)
 export class OtherReasonController extends BaseHttpController {
+    constructor(@inject(RedisService) private readonly redisService: RedisService) {
+        super();
+    }
 
     @httpGet('')
-    public renderForm(): void {
-        this.httpContext.response.render('other-reason');
+    public async renderForm(): Promise<void> {
+        const session = await this.redisService.get(sessionKey);
+
+        return this.render(session ? JSON.parse(session) : {})
     }
 
     @httpPost('')
-    public handleFormSubmission(): void {
+    public async handleFormSubmission(): Promise<void> {
         const body: OtherReason = this.httpContext.request.body;
 
         const validationResult: ValidationResult = new SchemaValidator(schema).validate(body);
 
-        this.httpContext.response.render('other-reason', {...body, validationResult});
+        if (validationResult.errors.length === 0) {
+            await this.redisService.set(sessionKey, JSON.stringify(body))
+        }
+
+        return this.render({...body, validationResult});
+    }
+
+    private async render(data: any): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            this.httpContext.response.render('other-reason', data, (err, compiled) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(compiled as any);
+            })
+        });
     }
 }
