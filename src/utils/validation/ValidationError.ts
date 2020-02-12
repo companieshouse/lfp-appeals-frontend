f2ff43e4df8ea9069bb319d51a0016eb2e491d75
Damian Dunajski <ddunajski@companieshouse.gov.uk>
2020-02-12 13:53:16 +0000

    Add validator tests

--- src/utils/validation/ValidationError.ts
+++ src/utils/validation/ValidationError.ts
@@ -1,5 +1,12 @@
 export class ValidationError {
-    constructor (public readonly field: string, public readonly text: string) {}
+    constructor (public readonly field: string, public readonly text: string) {
+        if (!field) {
+            throw new Error('Field name is required')
+        }
+        if (!text) {
+            throw new Error('Error message is required')
+        }
+    }
 
     get href(): string {
         return `#${this.field}-error`;
