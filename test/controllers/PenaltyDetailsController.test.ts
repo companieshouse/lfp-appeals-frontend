64f799d572afb7f57a12eaf3e11b84211f8d4dae
n-r-b <nblanchette@companieshouse.gov.uk>
2020-02-13 15:00:53 +0000

    feat: fixed controller tests and updated response code for invalid entry

--- test/controllers/PenaltyDetailsController.test.ts
+++ test/controllers/PenaltyDetailsController.test.ts
@@ -5,7 +5,8 @@ import { createSubstituteOf } from '../SubstituteFactory';
 
 import '../../src/controllers/PenaltyDetailsController';
 import { RedisService } from '../../src/services/RedisService';
-import { BAD_REQUEST, OK } from 'http-status-codes';
+import { MOVED_TEMPORARILY, OK, UNPROCESSABLE_ENTITY } from 'http-status-codes';
+import { expect } from 'chai';
 import { PenaltyReferenceDetails } from '../../src/models/PenaltyReferenceDetails';
 
 const app = createApplication(container => {
@@ -15,34 +16,74 @@ const app = createApplication(container => {
         }));
 });
 
+
 describe('PenaltyDetailsController', () => {
+    describe('GET request', () => {
 
-    it('should return 200 when trying to access the penalty-reference page', async () => {
-        await request(app)
-            .get('/penalty-reference')
-            .expect(OK);
+        it('should return 200 when trying to access the penalty-reference page without a session', async () => {
+            await request(app).get('/penalty-reference')
+                .expect(resonse => {
+                    expect(resonse.status).to.be.equal(OK)
+                })
+        });
+
+
+        it('should return 200 when trying to access the penalty-reference page with a session', async () => {
+
+            const penaltyDetails: PenaltyReferenceDetails = {
+                penaltyReference: 'A12345678',
+                companyNumber: 'SC123123'
+            };
+
+            const app = createApplication(container => {
+                container
+                    .bind(RedisService).toConstantValue(createSubstituteOf<RedisService>(service => {
+                        service.getObject('1').returns(Promise.resolve(penaltyDetails))
+                    }));
+            });
+            await request(app).get('/penalty-reference')
+                .set('Cookie', ['penalty-cookie=1'])
+                .expect(response => {
+                    expect(response.status).to.be.equal(OK)
+                    expect(response.text).to.contain(penaltyDetails.companyNumber)
+                    expect(response.text).to.contain(penaltyDetails.penaltyReference)
+                })
+        });
     });
 
-    // it('should return 200 when posting valid penalty details', async () => {
-    //     const penaltyDetails: PenaltyReferenceDetails = {
-    //         penaltyReference: 'A12345678',
-    //         companyNumber: 'SC123123'
-    //     };
-    //     await request(app)
-    //         .post('/penalty-reference')
-    //         .send(penaltyDetails)
-    //         .expect(OK);
-    // });
+    describe('POST request', () => {
+        it('should return 200 when posting valid penalty details', async () => {
 
-    it('should return 400 when posting empty penalty reference', async () => {
-        const penaltyDetails: PenaltyReferenceDetails = {
-            penaltyReference: '',
-            companyNumber: 'SC123123'
-        };
-        await request(app)
-            .post('/penalty-reference')
-            .send(penaltyDetails)
-            .expect(BAD_REQUEST);
+            const penaltyDetails: PenaltyReferenceDetails = {
+                penaltyReference: 'A12345678',
+                companyNumber: 'SC123123'
+            };
+
+            const app = createApplication(container => {
+                container
+                    .bind(RedisService).toConstantValue(createSubstituteOf<RedisService>(service => {
+                        service.setObject('1', penaltyDetails).returns(Promise.resolve('{}'))
+                    }));
+            });
+
+            await request(app).post('/penalty-reference')
+                .send(penaltyDetails)
+                .expect(response => {
+                    expect(response.status).to.be.equal(MOVED_TEMPORARILY)
+                });
+
+        });
+
+        it('should return 400 when posting empty penalty reference', async () => {
+
+            const penaltyDetails: PenaltyReferenceDetails = {
+                penaltyReference: '',
+                companyNumber: 'SC123123'
+            };
+            await request(app).post('/penalty-reference')
+                .send(penaltyDetails)
+                .expect(UNPROCESSABLE_ENTITY);
+        });
+
     });
-
-})
+});
