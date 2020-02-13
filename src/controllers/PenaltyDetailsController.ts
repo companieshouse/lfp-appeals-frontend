be46b9bbdd3c52f93fa4476395cb9074ae8a3af1
n-r-b <nblanchette@companieshouse.gov.uk>
2020-02-13 09:52:59 +0000

    refactor: tidied up penalty details controller

--- src/controllers/PenaltyDetailsController.ts
+++ src/controllers/PenaltyDetailsController.ts
@@ -1,5 +1,4 @@
-import { Request, Response } from 'express';
-import { controller, httpGet, httpPost, request, response } from 'inversify-express-utils';
+import { controller, httpGet, httpPost } from 'inversify-express-utils';
 import { inject } from 'inversify';
 import { BAD_REQUEST } from 'http-status-codes';
 import { PENALTY_DETAILS_PREFIX } from '../utils/Paths';
@@ -27,76 +26,69 @@ export class PenaltyDetailsController extends BaseAsyncHttpController {
     }
 
     @httpGet('')
-    public async getPenaltyDetailsView(@request() req: Request, @response() res: Response): Promise<string> {
+    public async getPenaltyDetailsView(): Promise<string> {
 
-        const cookieId: string = req.cookies[this.COOKIE_NAME];
+        const cookieId: string = this.httpContext.request.cookies[this.COOKIE_NAME];
+
+        let body: PenaltyReferenceDetails = {
+            companyNumber: '',
+            penaltyReference: ''
+        }
 
         if (cookieId) {
-
-            console.log('Cookie found, loading session ...');
 
             const data: IMap<any> = await this.redisService.getObject(cookieId);
 
             if (!data) {
-                console.log('No data found')
-                res.cookie(this.COOKIE_NAME, cookieId, { expires: new Date(Date.now()) })
+                this.httpContext.response.cookie(this.COOKIE_NAME, cookieId, { expires: new Date(Date.now()) })
             } else {
-                const body: PenaltyReferenceDetails = {
+                body = {
                     companyNumber: data[this.COMPANY_NUMBER],
                     penaltyReference: data[this.PENALTY_REFERENCE]
                 }
-
-                return this.render(this.PENALTY_TEMPLATE, { ...body });
             }
         }
 
-        return this.render(this.PENALTY_TEMPLATE);
+        return this.render(this.PENALTY_TEMPLATE, { ...body });
 
     }
 
     @httpPost('')
-    public async createPenaltyDetails(@request() req: Request, @response() res: Response): Promise<any> {
+    public async createPenaltyDetails(): Promise<any> {
+
+        const request = this.httpContext.request;
 
         const body: PenaltyReferenceDetails = {
-            companyNumber: sanitize(req.body.companyNumber),
-            penaltyReference: req.body.penaltyReference
+            companyNumber: sanitize(request.body.companyNumber),
+            penaltyReference: request.body.penaltyReference
         }
 
-        const validator = new SchemaValidator(penaltyDetailsSchema);
-
-        const validationResult: ValidationResult =   validator.validate(body);
+        const validationResult: ValidationResult = new SchemaValidator(penaltyDetailsSchema).validate(body);
 
         console.log(validationResult)
 
-        if (validationResult.errors.length < 1) {
+        if (validationResult.errors.length > 0) {
 
-            let cookieId: string = req.cookies[this.COOKIE_NAME];
-
-            const data: IMap<any> = {
-                penaltyReference: body.penaltyReference,
-                companyNumber: body.companyNumber
-            }
-
-            if (!cookieId) {
-
-                cookieId = this.generateCookieId();
-                res.cookie(this.COOKIE_NAME, this.COOKIE_ID);
-            }
-
-            await this.redisService.setObject(cookieId, data).then(v => {
-                console.log('Updated session')
-                console.log(validationResult)
-
-            });
-            return this.redirect(PENALTY_DETAILS_PREFIX).executeAsync();
-
-
-        } else {
-            console.log('Errors found')
             return await this.renderWithStatus(BAD_REQUEST)(
                 this.PENALTY_TEMPLATE, { cache: false, ...body, validationResult });
         }
 
+        let cookieId: string = request.cookies[this.COOKIE_NAME];
+
+        if (!cookieId) {
+
+            cookieId = this.generateCookieId();
+            this.httpContext.response.cookie(this.COOKIE_NAME, this.COOKIE_ID);
+        }
+
+        const data: IMap<any> = {
+            penaltyReference: body.penaltyReference,
+            companyNumber: body.companyNumber
+        }
+
+        await this.redisService.setObject(cookieId, data);
+
+        return this.redirect(PENALTY_DETAILS_PREFIX).executeAsync();
     }
 
     generateCookieId(): string {
