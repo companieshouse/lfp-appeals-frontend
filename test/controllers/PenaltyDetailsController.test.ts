de7b8f88eb9980e2c0cd9b590bbeb19c5fba32e4
Damian Dunajski <ddunajski@companieshouse.gov.uk>
2020-02-14 11:16:58 +0000

    Cleanup controller integration tests

--- test/controllers/PenaltyDetailsController.test.ts
+++ test/controllers/PenaltyDetailsController.test.ts
@@ -8,126 +8,129 @@ import { RedisService } from '../../src/services/RedisService';
 import { MOVED_TEMPORARILY, OK, UNPROCESSABLE_ENTITY } from 'http-status-codes';
 import { expect } from 'chai';
 import { PenaltyReferenceDetails } from '../../src/models/PenaltyReferenceDetails';
+import { PENALTY_DETAILS_PREFIX } from '../../src/utils/Paths'
+
+const pageHeading = 'What are the penalty details?';
+const errorSummaryHeading = 'There is a problem with the information you entered';
 
 const app = createApplication(container => {
-    container
-        .bind(RedisService).toConstantValue(createSubstituteOf<RedisService>(service => { service }));
+    container.bind(RedisService).toConstantValue(createSubstituteOf<RedisService>());
 });
-
 
 describe('PenaltyDetailsController', () => {
     describe('GET request', () => {
 
-        it('should return 200 when trying to access the penalty-reference page without a session', async () => {
-            await request(app).get('/penalty-reference')
-                .expect(resonse => {
-                    expect(resonse.status).to.be.equal(OK)
+        it('should return 200 when trying to access page without a session', async () => {
+            await request(app).get(PENALTY_DETAILS_PREFIX)
+                .expect(response => {
+                    expect(response.status).to.be.equal(OK);
+                    expect(response.text).to.contain(pageHeading)
+                        .and.not.contain(errorSummaryHeading);
                 })
         });
 
-
-        it('should return 200 when trying to access the penalty-reference page with a session', async () => {
-
+        it('should return 200 when trying to access page with a session', async () => {
             const penaltyDetails: Record<string, any> = {
                 penaltyReference: 'A12345678',
                 companyNumber: 'SC123123'
             };
 
             const app = createApplication(container => {
-                container
-                    .bind(RedisService).toConstantValue(createSubstituteOf<RedisService>(service => {
-                        service.getObject('1').returns(Promise.resolve(penaltyDetails))
-                    }));
+                container.bind(RedisService).toConstantValue(createSubstituteOf<RedisService>(service => {
+                    service.getObject('1').returns(Promise.resolve(penaltyDetails))
+                }));
             });
-            await request(app).get('/penalty-reference')
+
+            await request(app).get(PENALTY_DETAILS_PREFIX)
                 .set('Cookie', ['penalty-cookie=1'])
                 .expect(response => {
-                    expect(response.status).to.be.equal(OK)
-                    expect(response.text).to.contain(penaltyDetails.companyNumber)
-                    expect(response.text).to.contain(penaltyDetails.penaltyReference)
+                    expect(response.status).to.be.equal(OK);
+                    expect(response.text).to.contain(pageHeading)
+                        .and.to.contain(penaltyDetails.companyNumber)
+                        .and.to.contain(penaltyDetails.penaltyReference)
+                        .and.not.contain(errorSummaryHeading)
                 })
         });
     });
 
     describe('POST request', () => {
         it('should return 302 when posting valid penalty details', async () => {
-
             const penaltyDetails: PenaltyReferenceDetails = {
                 penaltyReference: 'A12345678',
                 companyNumber: 'SC123123'
             };
 
             const app = createApplication(container => {
-                container
-                    .bind(RedisService).toConstantValue(createSubstituteOf<RedisService>(service => {
-                        service.setObject('1', penaltyDetails).returns(Promise.resolve('{}'))
-                    }));
+                container.bind(RedisService).toConstantValue(createSubstituteOf<RedisService>(service => {
+                    service.setObject('1', penaltyDetails).returns(Promise.resolve())
+                }));
             });
 
-            await request(app).post('/penalty-reference')
+            await request(app).post(PENALTY_DETAILS_PREFIX)
                 .send(penaltyDetails)
                 .expect(response => {
-                    expect(response.get('Set-Cookie')).to.contain('penalty-cookie=1; Path=/')
-                    expect(response.status).to.be.equal(MOVED_TEMPORARILY)
+                    expect(response.get('Set-Cookie')).to.contain('penalty-cookie=1; Path=/');
+                    expect(response.status).to.be.equal(MOVED_TEMPORARILY);
                 });
-
         });
 
         it('should return 400 when posting empty penalty reference', async () => {
-
             const penaltyDetails: PenaltyReferenceDetails = {
                 penaltyReference: '',
                 companyNumber: 'SC123123'
             };
-            await request(app).post('/penalty-reference')
+
+            await request(app).post(PENALTY_DETAILS_PREFIX)
                 .send(penaltyDetails)
                 .expect(response => {
                     expect(response.status).to.be.equal(UNPROCESSABLE_ENTITY);
-                    expect(response.text).to.contain('You must enter a penalty reference number');
+                    expect(response.text).to.contain(pageHeading)
+                        .and.to.contain('You must enter a penalty reference number');
                 });
-
         });
 
         it('should return 400 when posting invalid penalty reference', async () => {
-
             const penaltyDetails: PenaltyReferenceDetails = {
                 penaltyReference: '12345678',
                 companyNumber: 'SC123123'
             };
-            await request(app).post('/penalty-reference')
+
+            await request(app).post(PENALTY_DETAILS_PREFIX)
                 .send(penaltyDetails)
                 .expect(response => {
                     expect(response.status).to.be.equal(UNPROCESSABLE_ENTITY);
-                    expect(response.text).to.contain('You must enter your reference number exactly as shown on your penalty notice');
+                    expect(response.text).to.contain(pageHeading)
+                        .and.to.contain('You must enter your reference number exactly as shown on your penalty notice');
                 });
-
         });
 
         it('should return 400 when posting empty company number', async () => {
-
             const penaltyDetails: PenaltyReferenceDetails = {
                 penaltyReference: 'A12345678',
                 companyNumber: ''
             };
-            await request(app).post('/penalty-reference')
+
+            await request(app).post(PENALTY_DETAILS_PREFIX)
                 .send(penaltyDetails)
                 .expect(response => {
                     expect(response.status).to.be.equal(UNPROCESSABLE_ENTITY);
-                    expect(response.text).to.contain('You must enter a company number');
+                    expect(response.text).to.contain(pageHeading)
+                        .and.to.contain('You must enter a company number');
                 });
         });
 
         it('should return 400 when posting invalid company number', async () => {
-
             const penaltyDetails: PenaltyReferenceDetails = {
                 penaltyReference: 'A12345678',
                 companyNumber: 'AB66666666'
             };
-            await request(app).post('/penalty-reference')
+
+            await request(app).post(PENALTY_DETAILS_PREFIX)
                 .send(penaltyDetails)
                 .expect(response => {
                     expect(response.status).to.be.equal(UNPROCESSABLE_ENTITY);
-                    expect(response.text).to.contain('You must enter your full eight character company number');
+                    expect(response.text).to.contain(pageHeading)
+                        .and.to.contain('You must enter your full eight character company number');
                 });
         });
     });
