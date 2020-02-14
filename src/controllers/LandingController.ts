125546b2f8f340db548f95c4373f40b829ae4601
akaJSaunders <jsaunders1@companieshouse.gov.uk>
2020-02-14 13:31:29 +0000

    refactor: added post request rather than redirect through template

--- src/controllers/LandingController.ts
+++ src/controllers/LandingController.ts
@@ -1,11 +1,16 @@
 import { Request, Response, NextFunction } from 'express';
-import { controller, httpGet } from 'inversify-express-utils';
+import { controller, httpGet, httpPost, BaseHttpController } from 'inversify-express-utils';
 
 @controller('/')
-export class LandingController {
+export class LandingController extends BaseHttpController {
 
     @httpGet('')
     public renderView(req: Request, res: Response, next: NextFunction): void {
         res.render('landing');
+    }
+
+    @httpPost('')
+    public continue(): void {
+        this.httpContext.response.redirect('/penalty-reference');
     }
 }
