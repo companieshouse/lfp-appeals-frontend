f41809bc25cfacc86bae1611e2f3ff8fae6287b2
Natalie Blanchette <16392604+n-r-b@users.noreply.github.com>
2020-02-14 15:35:22 +0000

    feat: link between disclaimer page and other reason details page (#31)

--- src/controllers/OtherReasonDisclaimerController.ts
+++ src/controllers/OtherReasonDisclaimerController.ts
@@ -1,14 +1,19 @@
-import { controller, BaseHttpController, httpGet } from 'inversify-express-utils';
-import { OTHER_REASON_CATEGORY_PREFIX, OTHER_REASON_DISCLAIMER_PAGE_PREFIX } from '../utils/Paths';
+import { controller, BaseHttpController, httpGet, httpPost } from 'inversify-express-utils';
+import { OTHER_REASON_PAGE_URI, OTHER_REASON_DISCLAIMER_PAGE_URI } from '../utils/Paths';
 
-@controller(OTHER_REASON_CATEGORY_PREFIX)
+@controller(OTHER_REASON_DISCLAIMER_PAGE_URI)
 export class OtherReasonDisclaimerController extends BaseHttpController {
     constructor() {
         super();
     }
 
-    @httpGet(OTHER_REASON_DISCLAIMER_PAGE_PREFIX)
+    @httpGet('')
     public showDisclaimer(): void {
         this.httpContext.response.render('other-reason-disclaimer');
+    }
+
+    @httpPost('')
+    public continue(): void {
+        this.httpContext.response.redirect(OTHER_REASON_PAGE_URI);
     }
 }
