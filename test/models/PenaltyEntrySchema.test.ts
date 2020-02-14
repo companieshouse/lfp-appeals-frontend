179d321ffa4d27dc41a35d84bafdd1cc01fff9e6
Damian Dunajski <ddunajski@companieshouse.gov.uk>
2020-02-14 11:50:32 +0000

    Refoactor validation test to make it more readable

--- test/models/PenaltyEntrySchema.test.ts
+++ test/models/PenaltyEntrySchema.test.ts
@@ -1,4 +1,3 @@
-
 import { expect } from 'chai';
 import { SchemaValidator } from '../../src/utils/validation/SchemaValidator';
 import { PenaltyReferenceDetails } from '../../src/models/PenaltyReferenceDetails';
@@ -10,240 +9,173 @@ describe('Penalty Details Schema Validation', () => {
     const validator = new SchemaValidator(schema);
 
     describe('Company Number', () => {
+        function createModelWithCompanyNumber(companyNumber: string): PenaltyReferenceDetails {
+            const validPenaltyReference = 'A12345678';
+            return { companyNumber, penaltyReference: validPenaltyReference }
+        }
+
         describe('Happy path', () => {
-
             it('should accept SC leading characters input', () => {
-
-                const penaltyDetails: PenaltyReferenceDetails = {
-                    penaltyReference: 'A12345678',
-                    companyNumber: 'SC123123'
-                };
-                const result = validator.validate(penaltyDetails)
-                const expectedResult = {errors: []}
-                expect(result).to.deep.equal(expectedResult)
+                const result = validator.validate(createModelWithCompanyNumber('SC123123'));
+                expect(result).to.deep.equal({errors: []})
             });
 
             it('should accept NI leading characters input', () => {
-
-                const penaltyDetails: PenaltyReferenceDetails = {
-                    penaltyReference: 'A12345678',
-                    companyNumber: 'NI123123'
-                };
-                const result = validator.validate(penaltyDetails)
-                const expectedResult = {errors: []}
-                expect(result).to.deep.equal(expectedResult)
+                const result = validator.validate(createModelWithCompanyNumber('NI123123'));
+                expect(result).to.deep.equal({errors: []})
             });
 
             it('should accept no leading characters input', () => {
-
-                const penaltyDetails: PenaltyReferenceDetails = {
-                    penaltyReference: 'A12345678',
-                    companyNumber: '12123123'
-                };
-                const result = validator.validate(penaltyDetails)
-                const expectedResult = {errors: []}
-                expect(result).to.deep.equal(expectedResult)
+                const result = validator.validate(createModelWithCompanyNumber('12123123'));
+                expect(result).to.deep.equal({errors: []})
             });
 
             it('should accept leading characters in lowercase', () => {
-
-                const penaltyDetails: PenaltyReferenceDetails = {
-                    penaltyReference: 'A12345678',
-                    companyNumber: 'sc123123'
-                };
-                const result = validator.validate(penaltyDetails)
-                const expectedResult = {errors: []}
-                expect(result).to.deep.equal(expectedResult)
+                const result = validator.validate(createModelWithCompanyNumber('sc123123'));
+                expect(result).to.deep.equal({errors: []})
             });
 
             it('should accept company numbers with less than 8 total characters', () => {
-
-                const penaltyDetails: PenaltyReferenceDetails = {
-                    penaltyReference: 'L12345678',
-                    companyNumber: '123'
-                };
-                const result = validator.validate(penaltyDetails)
-                const expectedResult = {errors:[]}
-                expect(result).to.deep.equal(expectedResult)
+                const result = validator.validate(createModelWithCompanyNumber('123'));
+                expect(result).to.deep.equal({errors: []})
             });
-        })
+        });
 
         describe('Bad path', () => {
             it('should reject empty field', () => {
-
-                const penaltyDetails: PenaltyReferenceDetails = {
-                    penaltyReference: 'A12345678',
-                    companyNumber: ''
-                };
-                const result = validator.validate(penaltyDetails)
-                const expectedResult = {
-                    errors:[{field: 'companyNumber',text : 'You must enter a company number'}]
-                }
-                expect(result).to.deep.equal(expectedResult)
+                const result = validator.validate(createModelWithCompanyNumber(''));
+                expect(result).to.deep.equal({
+                    errors: [{
+                        field: 'companyNumber',
+                        text: 'You must enter a company number'
+                    }]
+                })
             });
 
             it('should reject company number without correct leading characters', () => {
-
-                const penaltyDetails: PenaltyReferenceDetails = {
-                    penaltyReference: 'H12345678',
-                    companyNumber: 'S1231231'
-                };
-                const result = validator.validate(penaltyDetails)
-                const expectedResult = {
-                        errors:[{field: 'companyNumber', text : 'You must enter your full eight character company number'}]
-                }
-                expect(result).to.deep.equal(expectedResult)
+                const result = validator.validate(createModelWithCompanyNumber('S1231231'));
+                expect(result).to.deep.equal({
+                    errors: [{
+                        field: 'companyNumber',
+                        text: 'You must enter your full eight character company number'
+                    }]
+                })
             });
 
             it('should reject company numbers with letters mid-number', () => {
-
-                const penaltyDetails: PenaltyReferenceDetails = {
-                    penaltyReference: 'L12345678',
-                    companyNumber: '123SC123'
-                };
-                const result = validator.validate(penaltyDetails)
-                const expectedResult = {
-                        errors:[{field: 'companyNumber', text : 'You must enter your full eight character company number'}]
-                }
-                expect(result).to.deep.equal(expectedResult)
+                const result = validator.validate(createModelWithCompanyNumber('123SC123'));
+                expect(result).to.deep.equal({
+                    errors: [{
+                        field: 'companyNumber',
+                        text: 'You must enter your full eight character company number'
+                    }]
+                })
             });
 
             it('should reject empty spaces', () => {
-
-                const penaltyDetails: PenaltyReferenceDetails = {
-                    penaltyReference: 'L12345678',
-                    companyNumber: '  '
-                };
-                const result = validator.validate(penaltyDetails)
-                const expectedResult = {
-                        errors:[{field: 'companyNumber', text : 'You must enter your full eight character company number'}]
-                }
-                expect(result).to.deep.equal(expectedResult)
+                const result = validator.validate(createModelWithCompanyNumber('  '));
+                expect(result).to.deep.equal({
+                    errors: [{
+                        field: 'companyNumber',
+                        text: 'You must enter your full eight character company number'
+                    }]
+                })
             });
 
             it('should reject company numbers with spaces', () => {
-
-                const penaltyDetails: PenaltyReferenceDetails = {
-                    penaltyReference: 'L12345678',
-                    companyNumber: 'SC 12 34 56'
-                };
-                const result = validator.validate(penaltyDetails)
-                const expectedResult = {
-                        errors:[{field: 'companyNumber', text : 'You must enter your full eight character company number'}]
-                }
-                expect(result).to.deep.equal(expectedResult)
+                const result = validator.validate(createModelWithCompanyNumber('SC 12 34 56'));
+                expect(result).to.deep.equal({
+                    errors: [{
+                        field: 'companyNumber',
+                        text: 'You must enter your full eight character company number'
+                    }]
+                })
             });
 
             it('should reject symbols in company number', () => {
-
-                const penaltyDetails: PenaltyReferenceDetails = {
-                    penaltyReference: 'L12345678',
-                    companyNumber: 'SC12$$56'
-                };
-                const result = validator.validate(penaltyDetails)
-                const expectedResult = {
-                        errors:[{field: 'companyNumber', text : 'You must enter your full eight character company number'}]
-                }
-                expect(result).to.deep.equal(expectedResult)
+                const result = validator.validate(createModelWithCompanyNumber('SC12$$56'));
+                expect(result).to.deep.equal({
+                    errors: [{
+                        field: 'companyNumber',
+                        text: 'You must enter your full eight character company number'
+                    }]
+                })
             });
-
-
         })
-    })
+    });
 
     describe('Penalty Reference', () => {
+        function createModelWithPenaltyReference(penaltyReference: string): PenaltyReferenceDetails {
+            const validCompanyNumber = 'SC123123';
+            return { penaltyReference, companyNumber: validCompanyNumber }
+        }
+
         describe('Happy path', () => {
             it('should accept leading character', () => {
-
-                const penaltyDetails: PenaltyReferenceDetails = {
-                    penaltyReference: 'Z12345678',
-                    companyNumber: 'NI123123'
-                };
-                const result = validator.validate(penaltyDetails)
-                const expectedResult = {errors: []}
-                expect(result).to.deep.equal(expectedResult)
+                const result = validator.validate(createModelWithPenaltyReference('Z12345678'));
+                expect(result).to.deep.equal({errors: []})
             });
-
-        })
+        });
 
         describe('Bad path', () => {
-
             it('should reject empty field', () => {
-                const penaltyDetails: PenaltyReferenceDetails = {
-                    penaltyReference: '',
-                    companyNumber: 'SC123123'
-                };
-                const result = validator.validate(penaltyDetails)
-                const expectedResult = {
-                    errors:[{field: 'penaltyReference',text : 'You must enter a penalty reference number'}]
-                }
-                expect(result).to.deep.equal(expectedResult)
+                const result = validator.validate(createModelWithPenaltyReference(''));
+                expect(result).to.deep.equal({
+                    errors: [{
+                        field: 'penaltyReference',
+                        text: 'You must enter a penalty reference number'
+                    }]
+                })
             });
 
             it('should reject correct penalty reference in lowercase', () => {
-
-                const penaltyDetails: PenaltyReferenceDetails = {
-                    penaltyReference: 'b12345678',
-                    companyNumber: 'NI123123'
-                };
-                const result = validator.validate(penaltyDetails)
-                const expectedResult = {
-                    errors:[{field: 'penaltyReference', text: 'You must enter your reference number exactly as shown on your penalty notice'}]
-                }
-                expect(result).to.deep.equal(expectedResult)
+                const result = validator.validate(createModelWithPenaltyReference('b12345678'));
+                expect(result).to.deep.equal({
+                    errors: [{
+                        field: 'penaltyReference',
+                        text: 'You must enter your reference number exactly as shown on your penalty notice'
+                    }]
+                })
             });
 
             it('should reject penalty reference number missing leading character', () => {
-
-                const penaltyDetails: PenaltyReferenceDetails = {
-                    penaltyReference: '12345678',
-                    companyNumber: 'SC123123'
-                };
-                const result = validator.validate(penaltyDetails)
-                const expectedResult = {
-                        errors:[{field: 'penaltyReference', text: 'You must enter your reference number exactly as shown on your penalty notice'}]
-                }
-                expect(result).to.deep.equal(expectedResult)
+                const result = validator.validate(createModelWithPenaltyReference('12345678'));
+                expect(result).to.deep.equal({
+                    errors: [{
+                        field: 'penaltyReference',
+                        text: 'You must enter your reference number exactly as shown on your penalty notice'
+                    }]
+                })
             });
 
             it('should reject penalty references with less than 9 total characters', () => {
-
-                const penaltyDetails: PenaltyReferenceDetails = {
-                    penaltyReference: 'L123456',
-                    companyNumber: '123'
-                };
-                const result = validator.validate(penaltyDetails)
-                const expectedResult = {
-                        errors:[
-                            {field: 'penaltyReference', text: 'You must enter your reference number exactly as shown on your penalty notice'}]
-                }
-                expect(result).to.deep.equal(expectedResult)
+                const result = validator.validate(createModelWithPenaltyReference('L123456'));
+                expect(result).to.deep.equal({
+                    errors: [{
+                        field: 'penaltyReference',
+                        text: 'You must enter your reference number exactly as shown on your penalty notice'
+                    }]
+                })
             });
 
             it('should reject spaces in penalty references', () => {
-
-                const penaltyDetails: PenaltyReferenceDetails = {
-                    penaltyReference: 'L12 34 56 78',
-                    companyNumber: '12312312'
-                };
-                const result = validator.validate(penaltyDetails)
-                const expectedResult = {
-                        errors:[{field: 'penaltyReference', text: 'You must enter your reference number exactly as shown on your penalty notice'}]
-                }
-                expect(result).to.deep.equal(expectedResult)
+                const result = validator.validate(createModelWithPenaltyReference('L12 34 56 78'));
+                expect(result).to.deep.equal({
+                    errors: [{
+                        field: 'penaltyReference',
+                        text: 'You must enter your reference number exactly as shown on your penalty notice'
+                    }]
+                })
             });
 
             it('should reject symbols in penalty reference', () => {
-
-                const penaltyDetails: PenaltyReferenceDetails = {
-                    penaltyReference: 'L12*45678',
-                    companyNumber: '12345678'
-                };
-                const result = validator.validate(penaltyDetails)
-                const expectedResult = {
-                        errors:[{field: 'penaltyReference', text: 'You must enter your reference number exactly as shown on your penalty notice'}]
-                }
-                expect(result).to.deep.equal(expectedResult)
+                const result = validator.validate(createModelWithPenaltyReference('L12*45678'));
+                expect(result).to.deep.equal({
+                    errors: [{
+                        field: 'penaltyReference',
+                        text: 'You must enter your reference number exactly as shown on your penalty notice'
+                    }]
+                })
             });
         })
     })
