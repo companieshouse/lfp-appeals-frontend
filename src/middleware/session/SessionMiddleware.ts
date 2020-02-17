63e26cd7acac00ddc711ba976a9c9c99b1031e23
Francisco <fgoinhas@companieshouse.gov.uk>
2020-02-17 11:47:59 +0000

    refactor: Changes affected by node session api change

--- src/middleware/session/SessionMiddleware.ts
+++ src/middleware/session/SessionMiddleware.ts
@@ -3,17 +3,17 @@ import { provide } from 'inversify-binding-decorators';
 import { Response, Request, NextFunction, RequestHandler } from 'express';
 import { inject } from 'inversify';
 import { RedisService } from '../../services/RedisService';
-import { CHSessionMiddleware } from 'ch-node-session'
+import { CHSessionServiceInstance, CHSessionService } from 'ch-node-session'
 import { ENV } from '../../utils/ConfigLoader';
 
 @provide(SessionMiddleware)
 export class SessionMiddleware extends BaseMiddleware {
 
-    private sessionRequestHandler: RequestHandler;
+    private sessionRequestHandler: CHSessionService;
 
     public constructor(@inject(RedisService) private readonly redisService: RedisService) {
         super();
-        this.sessionRequestHandler = CHSessionMiddleware({
+        this.sessionRequestHandler = CHSessionServiceInstance({
             cacheDB: ENV.CACHE_DB,
             cachePassword: ENV.CACHE_PASSWORD,
             cacheServer: ENV.CACHE_SERVER,
@@ -25,7 +25,7 @@ export class SessionMiddleware extends BaseMiddleware {
 
     public async handler(req: Request, res: Response, next: NextFunction): Promise<void> {
 
-        return await this.sessionRequestHandler(req, res, next)
+        return await this.sessionRequestHandler.sessionHandler(req, res, next)
     }
 
 
