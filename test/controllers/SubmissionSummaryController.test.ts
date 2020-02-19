c8286e9946c738dc0fd71dd2e268fb6a2a05981c
akaJSaunders <jsaunders1@companieshouse.gov.uk>
2020-02-19 13:37:24 +0000

    test: add get request test for SubmissionSummaryController

---
+++ test/controllers/SubmissionSummaryController.test.ts
@@ -0,0 +1,20 @@
+import 'reflect-metadata'
+import '../../src/controllers/SubmissionSummaryController'
+import {createApplication} from "../ApplicationFactory";
+import {RedisService} from "../../src/services/RedisService";
+import {createSubstituteOf} from "../SubstituteFactory";
+import * as request from "supertest";
+import {SUBMISSION_SUMMARY_PAGE_URI} from "../../src/utils/Paths";
+import {OK} from "http-status-codes";
+
+
+describe('SubmissionSummaryController', () => {
+    describe('GET request', () => {
+        it('should return 200 when trying to access the submission summary', async () => {
+            const app = createApplication(container => {
+                container.bind(RedisService).toConstantValue(createSubstituteOf<RedisService>());
+            });
+            await request(app).get(SUBMISSION_SUMMARY_PAGE_URI).expect(OK);
+        });
+    });
+});
