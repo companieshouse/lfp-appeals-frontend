42d06345a85f10509de5e9f4053acc12fa2dcb03
n-r-b <nblanchette@companieshouse.gov.uk>
2020-02-19 15:06:07 +0000

    refactor: add return type to confirmation controller GET endpoint

--- src/controllers/ConfirmationController.ts
+++ src/controllers/ConfirmationController.ts
@@ -1,14 +1,14 @@
-import { controller, httpGet, BaseHttpController } from 'inversify-express-utils';
-import { CONFIRMATION_PAGE_URI } from '../utils/Paths';
+import {controller, httpGet, BaseHttpController} from 'inversify-express-utils';
+import {CONFIRMATION_PAGE_URI} from '../utils/Paths';
 
 @controller(CONFIRMATION_PAGE_URI)
 export class ConfirmationController extends BaseHttpController {
-    
+
     @httpGet('')
-    public getConfirmationView() {
+    public getConfirmationView(): void {
 
         const companyNumber: string = this.httpContext.request.app.locals.session.companyNumber;
-        
-        return this.httpContext.response.render('confirmation', { companyNumber });
+
+        this.httpContext.response.render('confirmation', {companyNumber});
     }
 }
