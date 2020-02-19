91574330b9685b78f54b5394528bb33815ecdfff
n-r-b <nblanchette@companieshouse.gov.uk>
2020-02-19 14:56:24 +0000

    refactor: removed creation of redis service from confirmation controller test

--- test/controllers/ConfirmationController.test.ts
+++ test/controllers/ConfirmationController.test.ts
@@ -2,16 +2,13 @@ import 'reflect-metadata'
 import {createApplication} from '../ApplicationFactory';
 import * as request from 'supertest'
 import {createSubstituteOf} from '../SubstituteFactory';
+import {expect} from 'chai';
 
 import '../../src/controllers/ConfirmationController';
-import {RedisService} from '../../src/services/RedisService';
 import {CONFIRMATION_PAGE_URI} from '../../src/utils/Paths';
-import {expect} from 'chai';
 import {OK} from 'http-status-codes';
 
-const app = createApplication(container => {
-    container.bind(RedisService).toConstantValue(createSubstituteOf<RedisService>());
-});
+const app = createApplication(container => {});
 
 describe('ConfirmationController', () => {
     describe('GET request', () => {
@@ -26,7 +23,7 @@ describe('ConfirmationController', () => {
 
                     expect(response.text).to.contain('Appeal submitted')
                         .and.to.contain(session.companyNumber);
-                    
+
                     expect(response.status).to.be.equal(OK);
                 })
         });
