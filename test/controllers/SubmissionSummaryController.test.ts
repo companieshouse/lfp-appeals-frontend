6ee8e52ccde4abed11ea8df436c505e55b4f5a0e
akaJSaunders <jsaunders1@companieshouse.gov.uk>
2020-02-19 15:15:41 +0000

    test: expect session information to be present in get request

--- test/controllers/SubmissionSummaryController.test.ts
+++ test/controllers/SubmissionSummaryController.test.ts
@@ -6,15 +6,36 @@ import {createSubstituteOf} from "../SubstituteFactory";
 import * as request from "supertest";
 import {SUBMISSION_SUMMARY_PAGE_URI} from "../../src/utils/Paths";
 import {OK} from "http-status-codes";
+import {expect} from 'chai';
 
+const app = createApplication(container => {
+    container.bind(RedisService).toConstantValue(createSubstituteOf<RedisService>());
+});
 
 describe('SubmissionSummaryController', () => {
     describe('GET request', () => {
         it('should return 200 when trying to access the submission summary', async () => {
-            const app = createApplication(container => {
-                container.bind(RedisService).toConstantValue(createSubstituteOf<RedisService>());
-            });
-            await request(app).get(SUBMISSION_SUMMARY_PAGE_URI).expect(OK);
+
+
+            const session = {
+                companyNumber: '00345567',
+                penaltyReference: 'A00000001',
+                email: 'joe@bloggs.mail',
+                reason: {
+                    otherReason: 'I have reasons',
+                    otherInformation: 'They are legit'
+                }
+            };
+
+            await request(app).get(SUBMISSION_SUMMARY_PAGE_URI)
+                .expect(response => {
+                    expect(response.text).to.contain(session.companyNumber);
+                    expect(response.text).to.contain(session.penaltyReference);
+                    expect(response.text).to.contain(session.email);
+                    expect(response.text).to.contain(session.reason.otherReason);
+                    expect(response.text).to.contain(session.reason.otherInformation);
+                    expect(response.status).to.be.equal(OK);
+                })
         });
     });
 });
