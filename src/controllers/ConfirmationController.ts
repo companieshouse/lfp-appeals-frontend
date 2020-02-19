a499972c69a20077f75f285b064961832caa431e
n-r-b <nblanchette@companieshouse.gov.uk>
2020-02-19 15:02:29 +0000

    refactor: extend BaseHttpController instead of Async version in confirmation controller

--- src/controllers/ConfirmationController.ts
+++ src/controllers/ConfirmationController.ts
@@ -1,9 +1,8 @@
-import { controller, httpGet } from 'inversify-express-utils';
+import { controller, httpGet, BaseHttpController } from 'inversify-express-utils';
 import { CONFIRMATION_PAGE_URI } from '../utils/Paths';
-import { BaseAsyncHttpController } from './BaseAsyncHttpController';
 
 @controller(CONFIRMATION_PAGE_URI)
-export class ConfirmationController extends BaseAsyncHttpController {
+export class ConfirmationController extends BaseHttpController {
     
     @httpGet('')
     public getConfirmationView() {
