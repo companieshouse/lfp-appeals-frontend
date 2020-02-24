68c7dd395377f39bb8291e17a590da7136fb6ff2
Francisco <fgoinhas@companieshouse.gov.uk>
2020-02-24 14:13:39 +0000

    refactor: Refactored AuthMiddleware test

--- test/ApplicationFactory.ts
+++ test/ApplicationFactory.ts
@@ -1,11 +1,11 @@
 import { Container } from 'inversify';
 import { Application, NextFunction, RequestHandler, Request, Response } from 'express';
 import { InversifyExpressServer, BaseMiddleware } from 'inversify-express-utils';
-import {getExpressAppConfig, loadEnvironmentVariablesFromFiles} from '../src/utils/ConfigLoader'
+import { getExpressAppConfig, loadEnvironmentVariablesFromFiles } from '../src/utils/ConfigLoader';
 import { AuthMiddleware } from '../src/middleware/AuthMiddleware';
 
-export const createApplication = (configureContainerBindings: (container: Container) => void = () => {}): Application => {
-    process.env.NODE_ENV = 'test';
+export const createApplication = (configureContainerBindings: (container: Container) => void = () => { }): Application => {
+
     loadEnvironmentVariablesFromFiles();
     const container = new Container();
     configureContainerBindings(container);
@@ -21,6 +21,6 @@ export const setupFakeAuth = (container: Container) => {
         handler: RequestHandler = fakeHandler;
     }
 
-    const newFakeMiddleware = new FakeMiddleware()
+    const newFakeMiddleware = new FakeMiddleware();
     container.bind<AuthMiddleware>(AuthMiddleware).toConstantValue(newFakeMiddleware);
-}
+};
