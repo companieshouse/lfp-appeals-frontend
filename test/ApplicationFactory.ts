9b097fd4e99e34d5dd623a60072b9fb6dd4777c9
akaJSaunders <jsaunders1@companieshouse.gov.uk>
2020-02-24 14:10:28 +0000

    fix: updated applicationFactory and Entry and health tests .env.test now required

--- test/ApplicationFactory.ts
+++ test/ApplicationFactory.ts
@@ -1,10 +1,12 @@
 import { Container } from 'inversify';
 import { Application, NextFunction, RequestHandler, Request, Response } from 'express';
 import { InversifyExpressServer, BaseMiddleware } from 'inversify-express-utils';
-import { getExpressAppConfig } from '../src/utils/ConfigLoader'
+import {getExpressAppConfig, loadEnvironmentVariablesFromFiles} from '../src/utils/ConfigLoader'
 import { AuthMiddleware } from '../src/middleware/AuthMiddleware';
 
 export const createApplication = (configureContainerBindings: (container: Container) => void = () => {}): Application => {
+    process.env.NODE_ENV = 'test';
+    loadEnvironmentVariablesFromFiles();
     const container = new Container();
     configureContainerBindings(container);
     return new InversifyExpressServer(container).setConfig(getExpressAppConfig('../../')).build();
--- test/controllers/EntryController.test.ts
+++ test/controllers/EntryController.test.ts
@@ -1,21 +1,39 @@
 import 'reflect-metadata';
 import '../../src/controllers/EntryController';
-import { createApplication } from '../ApplicationFactory';
+import {createApplication, setupFakeAuth} from '../ApplicationFactory';
 import * as request from 'supertest';
-import { createSubstituteOf } from '../SubstituteFactory';
-import { MOVED_TEMPORARILY  } from 'http-status-codes';
+import { MOVED_TEMPORARILY } from 'http-status-codes';
 import { ENTRY_PAGE_URI, PENALTY_DETAILS_PAGE_URI } from '../../src/utils/Paths';
 import { expect } from 'chai';
 import '../global';
-import { SessionStore } from 'ch-node-session';
+import { CookieConfig, SessionMiddleware, SessionStore } from 'ch-node-session';
+import { Redis } from "ioredis";
+import Substitute from "@fluffy-spoon/substitute";
+import { RequestHandler } from "express";
+import { returnEnvVarible } from "../../src/utils/ConfigLoader";
+
 
 describe('EntryController', () => {
 
     describe('GET request', () => {
         it('should return 302 when trying to access the entry page', async () => {
+
             const app = createApplication(container => {
-                container.bind(SessionStore).toConstantValue(createSubstituteOf<SessionStore>());
+
+                const config: CookieConfig = {
+                    cookieName: returnEnvVarible('COOKIE_NAME'),
+                    cookieSecret: returnEnvVarible('COOKIE_SECRET')
+                };
+
+                const redis = Substitute.for<Redis>();
+                const sessionStore = new SessionStore(redis);
+                const sessionHandler = SessionMiddleware(config, sessionStore);
+                setupFakeAuth(container);
+                container.bind<RequestHandler>(SessionMiddleware).toConstantValue(sessionHandler);
+                container.bind(SessionStore).toConstantValue(sessionStore);
+
             });
+
             await request(app).get(ENTRY_PAGE_URI).expect(response => {
                 expect(response.status).to.be.equal(MOVED_TEMPORARILY);
                 expect(response.get('Location')).to.be.equal(PENALTY_DETAILS_PAGE_URI);
