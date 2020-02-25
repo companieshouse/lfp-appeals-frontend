318c7dbe2f00b162b7df89377de703ae3c4ef975
Damian Dunajski <ddunajski@companieshouse.gov.uk>
2020-02-25 13:41:13 +0000

    Simplify health check test

--- test/controllers/HealthCheckController.test.ts
+++ test/controllers/HealthCheckController.test.ts
@@ -1,62 +1,35 @@
-import 'reflect-metadata';
-import {Application, RequestHandler} from 'express';
-import * as request from 'supertest';
-import {createApplication, setupFakeAuth} from '../ApplicationFactory';
-import '../../src/controllers/HealthCheckController';
-import { HEALTH_CHECK_URI } from '../../src/utils/Paths';
-import {CookieConfig, SessionMiddleware, SessionStore} from 'ch-node-session-handler';
-import { Redis } from 'ioredis';
-import { getEnvOrDefault } from '../../src/utils/EnvironmentUtils';
+import 'reflect-metadata'
+import { Application } from 'express'
+import * as request from 'supertest'
+import { createApplication } from '../ApplicationFactory';
+import { createSubstituteOf } from '../SubstituteFactory';
 
+import '../../src/controllers/HealthCheckController'
+import { Redis } from 'ioredis';
+import { SessionStore } from 'ch-node-session-handler';
+import { HEALTH_CHECK_URI } from '../../src/utils/Paths';
 
 describe('HealthCheckController', () => {
-
-
     it('should return 200 with status when redis database is up', async () => {
         const app = createApplication(container => {
-
-            const redis = {
-                ping: () => Promise.resolve('OK')
-            } as Redis;
-
-            const config: CookieConfig = {
-                cookieName: getEnvOrDefault('COOKIE_NAME'),
-                cookieSecret: getEnvOrDefault('COOKIE_SECRET')
-            };
-
-            const sessionStore = new SessionStore(redis);
-            const sessionHandler = SessionMiddleware(config, sessionStore);
-            setupFakeAuth(container);
-            container.bind<RequestHandler>(SessionMiddleware).toConstantValue(sessionHandler);
+            const redis = createSubstituteOf<Redis>((redis) => {
+                redis.ping().returns(Promise.resolve('OK'))
+            });
             container.bind(SessionStore).toConstantValue(new SessionStore(redis));
-
         });
 
-        await makeHealthCheckRequest(app).expect(200);
+        await makeHealthCheckRequest(app).expect(200, 'Redis status: 200');
     });
 
     it('should return 500 with status when redis database is down', async () => {
         const app = createApplication(container => {
-            const redis = {
-                ping: async () => new Promise(() => {
-                    throw Error();
-                })
-
-            } as Redis;
-
-            const config: CookieConfig = {
-                cookieName: getEnvOrDefault('COOKIE_NAME'),
-                cookieSecret: getEnvOrDefault('COOKIE_SECRET')
-            };
-
-            const sessionStore = new SessionStore(redis);
-            const sessionHandler = SessionMiddleware(config, sessionStore);
-            setupFakeAuth(container);
-            container.bind<RequestHandler>(SessionMiddleware).toConstantValue(sessionHandler);
+            const redis = createSubstituteOf<Redis>((redis) => {
+                redis.ping().returns(Promise.reject('ERROR'))
+            });
             container.bind(SessionStore).toConstantValue(new SessionStore(redis));
         });
 
-        await makeHealthCheckRequest(app).expect(500);
+        await makeHealthCheckRequest(app).expect(500, 'Redis status: 500');
     });
 
     function makeHealthCheckRequest(app: Application): request.Test {
