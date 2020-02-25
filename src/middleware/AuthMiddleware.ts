c5bb3249e92d1b53bde54645d3b97c9b4783905c
Damian Dunajski <ddunajski@companieshouse.gov.uk>
2020-02-25 12:41:56 +0000

    Replace REDIRECT_URL with ACCOUNT_URL

--- src/middleware/AuthMiddleware.ts
+++ src/middleware/AuthMiddleware.ts
@@ -1,11 +1,11 @@
 import { BaseMiddleware } from 'inversify-express-utils';
 import { Request, Response, NextFunction, RequestHandler } from 'express';
-import { returnEnvVarible } from "../utils/EnvironmentUtils";
+import { returnEnvVarible } from '../utils/EnvironmentUtils';
 import { ISignInInfo } from 'ch-node-session-handler/lib/session/model/SessionInterfaces';
 import { SessionKey } from 'ch-node-session-handler/lib/session/keys/SessionKey';
 import { SignInInfoKeys } from 'ch-node-session-handler/lib/session/keys/SignInInfoKeys';
 import { injectable } from 'inversify';
-import { ROOT_URI, PENALTY_DETAILS_PAGE_URI } from '../utils/Paths';
+import { PENALTY_DETAILS_PAGE_URI } from '../utils/Paths';
 import { VerifiedSession } from 'ch-node-session-handler/lib/session/model/Session';
 
 @injectable()
@@ -15,13 +15,12 @@ export class AuthMiddleware extends BaseMiddleware {
 
         req.session
             .chain((session: VerifiedSession) => session.getValue<ISignInInfo>(SessionKey.SignInInfo))
-            .filter((siginInInfo: ISignInInfo) => siginInInfo[SignInInfoKeys.SignedIn] === 1)
+            .filter((signInInfo: ISignInInfo) => signInInfo[SignInInfoKeys.SignedIn] === 1)
             .ifNothing(() => {
-                res.redirect(`${returnEnvVarible('REDIRECT_URL')}?return_to=${PENALTY_DETAILS_PAGE_URI}`);
+                res.redirect(`${returnEnvVarible('ACCOUNT_URL', '')}/signin?return_to=${PENALTY_DETAILS_PAGE_URI}`);
             });
 
         return next();
-
     };
 
 }
