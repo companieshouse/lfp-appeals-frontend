83e7f2e568f1fec5d86bef0ed5ffe2ee4f010394
Damian Dunajski <ddunajski@companieshouse.gov.uk>
2020-02-26 16:19:57 +0000

    Transit from summary page to confirmation page

--- src/controllers/CheckYourAppealController.ts
+++ src/controllers/CheckYourAppealController.ts
@@ -1,21 +1,20 @@
-import { controller, BaseHttpController, httpGet } from 'inversify-express-utils';
-import { SUBMISSION_SUMMARY_PAGE_URI } from '../utils/Paths';
+import { controller, httpGet, httpPost } from 'inversify-express-utils';
+import { SUBMISSION_SUMMARY_PAGE_URI, CONFIRMATION_PAGE_URI } from '../utils/Paths';
 import { SessionKey } from 'ch-node-session-handler/lib/session/keys/SessionKey';
 import { SignInInfoKeys } from 'ch-node-session-handler/lib/session/keys/SignInInfoKeys';
 import { ISignInInfo } from 'ch-node-session-handler/lib/session/model/SessionInterfaces';
 import { Request } from 'express';
 import { AuthMiddleware } from '../middleware/AuthMiddleware';
 import { SessionMiddleware, Maybe } from 'ch-node-session-handler';
-import { Appeal } from '../models/Appeal';
 import { AppealKeys } from '../models/keys/AppealKeys';
 import { BaseAsyncHttpController } from './BaseAsyncHttpController';
+import { HttpResponseMessage } from 'inversify-express-utils/dts/httpResponseMessage';
 
 @controller(SUBMISSION_SUMMARY_PAGE_URI, SessionMiddleware, AuthMiddleware)
 export class CheckYourAppealController extends BaseAsyncHttpController {
 
     @httpGet('')
     public async renderView(req: Request): Promise<string> {
-
         const userProfile = req.session
             .chain(_ => _.getValue<ISignInInfo>(SessionKey.SignInInfo))
             .map(info => info[SignInInfoKeys.UserProfile])
@@ -27,7 +26,10 @@ export class CheckYourAppealController extends BaseAsyncHttpController {
             .orDefault({});
 
         return this.render('check-your-appeal', { ...appealsData, userProfile });
+    }
 
-
+    @httpPost('')
+    public async handleFormSubmission(): Promise<HttpResponseMessage> {
+        return this.redirect(CONFIRMATION_PAGE_URI).executeAsync();
     }
 }
