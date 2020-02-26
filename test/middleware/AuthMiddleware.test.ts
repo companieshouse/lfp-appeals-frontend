b502e9b1b1f46b7b4fc63ada402ef434dfdb0b2d
Damian Dunajski <ddunajski@companieshouse.gov.uk>
2020-02-26 16:02:21 +0000

    Do not call next function when redirecting user agent to sign in page

--- test/middleware/AuthMiddleware.test.ts
+++ test/middleware/AuthMiddleware.test.ts
@@ -1,10 +1,13 @@
 import 'reflect-metadata';
 import '../../src/controllers/index';
-import { expect, assert } from 'chai';
+import { assert, expect } from 'chai';
 import * as request from 'supertest';
-import { PENALTY_DETAILS_PAGE_URI, OTHER_REASON_DISCLAIMER_PAGE_URI, OTHER_REASON_PAGE_URI } from '../../src/utils/Paths';
+import {
+    OTHER_REASON_DISCLAIMER_PAGE_URI,
+    OTHER_REASON_PAGE_URI,
+    PENALTY_DETAILS_PAGE_URI
+} from '../../src/utils/Paths';
 import { createApp, getDefaultConfig } from '../ApplicationFactory';
-import { Maybe, VerifiedSession } from 'ch-node-session-handler';
 import { createFakeSession } from '../utils/session/FakeSessionFactory';
 import { SessionKey } from 'ch-node-session-handler/lib/session/keys/SessionKey';
 import { generateSessionId, generateSignature } from 'ch-node-session-handler/lib/utils/CookieUtils';
@@ -74,54 +77,16 @@ describe('Authentication Middleware', () => {
 
         });
     });
-    describe('Unauthed path', () => {
-
-        const appWithoutCookie = { label: 'App without Cookie', app: createApp(session, false) };
-        const unAuthedApp = { label: 'App without Cookie and Session', app: createApp() };
-
-        const unAuthedAppStateVariations = [appWithoutCookie, unAuthedApp];
-
-        before('sign in server should be running', async () => {
-
-            await request(createApp())
-                .get(PENALTY_DETAILS_PAGE_URI)
-                .catch(err => assert.fail(err));
-        });
-
+    describe('no session scenario', () => {
         it('should redirect the user to sign in screen when trying to access protected pages', async () => {
-
-            for (const variation of unAuthedAppStateVariations) {
-                for (const page of protectedPages) {
-                    await request(variation.app).get(page)
-                        .expect(302).then(res => expect(res.header.location).to.include('/signin'));
-                }
-
-            }
-
-        });
-
-    });
-
-    describe('Auth Edge case', () => {
-
-        const appWithoutSessionObject = createApp(createFakeSession([], config.cookieSecret), true);
-
-        before('sign in server should be running', async () => {
-
-            await request(createApp())
-                .get(PENALTY_DETAILS_PAGE_URI)
-                .catch(err => assert.fail(err));
-        });
-
-        it('should allow user to access protected pages if session object is somehow lost.', async () => {
+            const appWithoutSession = createApp()
 
             for (const page of protectedPages) {
-                await request(appWithoutSessionObject).get(page)
-                    .expect(200);
+                await request(appWithoutSession).get(page)
+                    .expect(302).then(res => expect(res.header.location).to.include('/signin'));
             }
-
         });
-    });
 
+    });
 
 });
