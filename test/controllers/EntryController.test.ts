13243c184cf5d7f9bacab44a760a0552ae24af29
akaJSaunders <jsaunders1@companieshouse.gov.uk>
2020-02-26 14:35:31 +0000

    refactor: clean up entryController test

--- test/controllers/EntryController.test.ts
+++ test/controllers/EntryController.test.ts
@@ -1,20 +1,17 @@
 import 'reflect-metadata'
 import '../../src/controllers/EntryController'
-import { SessionStore } from 'ch-node-session-handler';
 import * as request from 'supertest'
-import { createSubstituteOf } from '../SubstituteFactory';
 import { MOVED_TEMPORARILY  } from 'http-status-codes';
 import { ENTRY_PAGE_URI, PENALTY_DETAILS_PAGE_URI } from '../../src/utils/Paths';
 import { expect } from 'chai';
 import { createApp, getDefaultConfig } from '../ApplicationFactory';
-import { createFakeSession } from '../utils/session/FakeSessionFactory';
 
 const config = getDefaultConfig()
 describe('EntryController', () => {
 
     describe('GET request', () => {
         it('should return 302 when trying to access the entry page', async () => {
-            const app = createApp(createFakeSession([], config.cookieSecret))
+            const app = createApp()
             await request(app).get(ENTRY_PAGE_URI).expect(response => {
                 expect(response.status).to.be.equal(MOVED_TEMPORARILY);
                 expect(response.get('Location')).to.be.equal(PENALTY_DETAILS_PAGE_URI);
