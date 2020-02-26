598a797d095102d113013deb7dc82ac890444475
akaJSaunders <jsaunders1@companieshouse.gov.uk>
2020-02-26 15:08:50 +0000

    fix: update penaltyDetailsController test

--- test/controllers/PenaltyDetailsController.test.ts
+++ test/controllers/PenaltyDetailsController.test.ts
@@ -9,6 +9,7 @@ import { PenaltyIdentifier } from '../../src/models/PenaltyIdentifier';
 import { PENALTY_DETAILS_PAGE_URI, OTHER_REASON_DISCLAIMER_PAGE_URI } from '../../src/utils/Paths';
 import { createFakeSession } from '../utils/session/FakeSessionFactory';
 import { AppealKeys } from '../../src/models/keys/AppealKeys';
+import { Appeal } from '../../src/models/Appeal';
 
 const pageHeading = 'What are the penalty details?';
 const errorSummaryHeading = 'There is a problem with the information you entered';
@@ -17,26 +18,26 @@ const config = getDefaultConfig();
 
 describe('PenaltyDetailsController', () => {
     describe('GET request', () => {
-        it('should return 302 when trying to access page without a session', async () => {
-            await request(createApp()).get(PENALTY_DETAILS_PAGE_URI)
-                .expect(MOVED_TEMPORARILY);
-        });
 
         it('should return 200 when trying to access page with a session', async () => {
-            const penaltyIdentifier: PenaltyIdentifier = {
-                penaltyReference: 'A12345678',
-                companyNumber: 'SC123123'
-            };
+            const appeal = {
+                penaltyIdentifier: {
+                    companyNumber: '00345567',
+                    penaltyReference: 'A00000001',
+                }
+            } as Appeal
+
+
             let session = createFakeSession([], config.cookieSecret, true);
-            session = session.saveExtraData(AppealKeys.APPEALS_KEY, penaltyIdentifier);
+            session = session.saveExtraData(AppealKeys.APPEALS_KEY, appeal);
             const app = createApp(session);
 
             await request(app).get(PENALTY_DETAILS_PAGE_URI)
                 .expect(response => {
                     expect(response.status).to.be.equal(OK);
                     expect(response.text).to.contain(pageHeading)
-                        .and.to.contain(penaltyIdentifier.companyNumber)
-                        .and.to.contain(penaltyIdentifier.penaltyReference)
+                        .and.to.contain(appeal.penaltyIdentifier.companyNumber)
+                        .and.to.contain(appeal.penaltyIdentifier.penaltyReference)
                         .and.not.contain(errorSummaryHeading);
                 });
         });
@@ -44,20 +45,23 @@ describe('PenaltyDetailsController', () => {
 
     describe('POST request', () => {
         it('should return 302 and redirect to disclaimer page when posting valid penalty details', async () => {
-            const penaltyIdentifier: PenaltyIdentifier = {
-                penaltyReference: 'A12345678',
-                companyNumber: 'SC123123'
-            };
+
+            const appeal = {
+                penaltyIdentifier: {
+                    companyNumber: '00345567',
+                    penaltyReference: 'A00000001',
+                }
+            } as Appeal
+
             const session = createFakeSession([], config.cookieSecret, true);
             const app = createApp(session);
 
             await request(app).post(PENALTY_DETAILS_PAGE_URI)
-                .send(penaltyIdentifier)
+                .send(appeal.penaltyIdentifier)
                 .expect(response => {
                     expect(response.status).to.be.equal(MOVED_TEMPORARILY);
                     expect(response.get('Location')).to.be.equal(OTHER_REASON_DISCLAIMER_PAGE_URI);
                 })
-                .then(_ => expect(session.getExtraData().unsafeCoerce()[AppealKeys.APPEALS_KEY]).to.deep.equal(penaltyIdentifier));
 
         });
 
