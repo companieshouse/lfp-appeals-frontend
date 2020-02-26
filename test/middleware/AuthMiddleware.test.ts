58d8f70f26e51a4e95a91b4182b2a522c0fbd6b4
akaJSaunders <jsaunders1@companieshouse.gov.uk>
2020-02-26 16:15:55 +0000

    fix: address code review and fix auth middleware test

--- test/middleware/AuthMiddleware.test.ts
+++ test/middleware/AuthMiddleware.test.ts
@@ -8,6 +8,7 @@ import { Maybe, VerifiedSession } from 'ch-node-session-handler';
 import { createFakeSession } from '../utils/session/FakeSessionFactory';
 import { SessionKey } from 'ch-node-session-handler/lib/session/keys/SessionKey';
 import { generateSessionId, generateSignature } from 'ch-node-session-handler/lib/utils/CookieUtils';
+import { PenaltyIdentifier } from '../../src/models/PenaltyIdentifier';
 
 const config = getDefaultConfig();
 const id = generateSessionId();
@@ -20,8 +21,6 @@ const session = createFakeSession(
         [SessionKey.ClientSig]: sig
     }], config.cookieSecret, true);
 
-const authedApp = createApp(session);
-
 const protectedPages = [
     PENALTY_DETAILS_PAGE_URI,
     OTHER_REASON_DISCLAIMER_PAGE_URI,
@@ -30,24 +29,14 @@ const protectedPages = [
 
 describe('Authentication Middleware', () => {
 
+    const authedApp = createApp(session);
 
     describe('Authed path', () => {
 
-        let penaltyDetails: Record<string, any>;
-
-        before('sign in server should be running', async () => {
-
-            penaltyDetails = {
-                penaltyReference: 'A12345678',
-                companyNumber: 'SC123123'
-            };
-
-            await request(createApp())
-                .get(PENALTY_DETAILS_PAGE_URI)
-                .catch(err => assert.fail(err));
-
-
-        });
+        const penaltyIdentifier: PenaltyIdentifier = {
+            penaltyReference: 'A12345678',
+            companyNumber: 'SC123123'
+        };
 
         it('should not redirect the user to the sign in page if the user is signed in', async () => {
 
@@ -58,70 +47,26 @@ describe('Authentication Middleware', () => {
 
         });
 
-        it('should allow the user to go to the penalty reference screen if authed', async () => {
-
-            await request(authedApp)
-                .get(PENALTY_DETAILS_PAGE_URI)
-                .expect(200);
-
-        });
         it('should continue to the next page without requiring auth if penalty details are valid', async () => {
 
             await request(authedApp)
                 .post(PENALTY_DETAILS_PAGE_URI)
-                .send(penaltyDetails)
+                .send(penaltyIdentifier)
                 .expect(_ => expect(_.header.location).to.include(OTHER_REASON_DISCLAIMER_PAGE_URI));
 
         });
     });
     describe('Unauthed path', () => {
 
-        const appWithoutCookie = { label: 'App without Cookie', app: createApp(session, false) };
-        const unAuthedApp = { label: 'App without Cookie and Session', app: createApp() };
-
-        const unAuthedAppStateVariations = [appWithoutCookie, unAuthedApp];
-
-        before('sign in server should be running', async () => {
-
-            await request(createApp())
-                .get(PENALTY_DETAILS_PAGE_URI)
-                .catch(err => assert.fail(err));
-        });
-
         it('should redirect the user to sign in screen when trying to access protected pages', async () => {
 
-            for (const variation of unAuthedAppStateVariations) {
-                for (const page of protectedPages) {
-                    await request(variation.app).get(page)
-                        .expect(302).then(res => expect(res.header.location).to.include('/signin'));
-                }
-
-            }
-
-        });
-
-    });
-
-    describe('Auth Edge case', () => {
-
-        const appWithoutSessionObject = createApp(createFakeSession([], config.cookieSecret), true);
-
-        before('sign in server should be running', async () => {
-
-            await request(createApp())
-                .get(PENALTY_DETAILS_PAGE_URI)
-                .catch(err => assert.fail(err));
-        });
-
-        it('should allow user to access protected pages if session object is somehow lost.', async () => {
+            const unAuthedApp = createApp()
 
             for (const page of protectedPages) {
-                await request(appWithoutSessionObject).get(page)
-                    .expect(200);
-            }
-
+                await request(unAuthedApp).get(page)
+                    .expect(302).then(res => expect(res.header.location).to.include('/signin'));
+                }
         });
+
     });
-
-
 });
