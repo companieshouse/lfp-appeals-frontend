1df3d15a1eaebc3d2d74f6c382b8571f074f294e
Francisco <fgoinhas@companieshouse.gov.uk>
2020-02-27 09:44:18 +0000

    test: Changed and added unit tests for AuthMiddleware as requested in PR

--- test/middleware/AuthMiddleware.test.ts
+++ test/middleware/AuthMiddleware.test.ts
@@ -5,13 +5,19 @@ import * as request from 'supertest';
 import {
     OTHER_REASON_DISCLAIMER_PAGE_URI,
     OTHER_REASON_PAGE_URI,
-    PENALTY_DETAILS_PAGE_URI
+    PENALTY_DETAILS_PAGE_URI,
+    CONFIRMATION_PAGE_URI,
+    SUBMISSION_SUMMARY_PAGE_URI
 } from '../../src/utils/Paths';
 import { createApp, getDefaultConfig } from '../ApplicationFactory';
 import { createFakeSession } from '../utils/session/FakeSessionFactory';
 import { SessionKey } from 'ch-node-session-handler/lib/session/keys/SessionKey';
 import { generateSessionId, generateSignature } from 'ch-node-session-handler/lib/utils/CookieUtils';
 import { PenaltyIdentifier } from '../../src/models/PenaltyIdentifier';
+import Substitute, { Arg } from '@fluffy-spoon/substitute';
+import { Request, Response, NextFunction } from 'express';
+import { AuthMiddleware } from '../../src/middleware/AuthMiddleware';
+import { Maybe } from 'ch-node-session-handler';
 
 const config = getDefaultConfig();
 const id = generateSessionId();
@@ -27,7 +33,9 @@ const session = createFakeSession(
 const protectedPages = [
     PENALTY_DETAILS_PAGE_URI,
     OTHER_REASON_DISCLAIMER_PAGE_URI,
-    OTHER_REASON_PAGE_URI
+    OTHER_REASON_PAGE_URI,
+    CONFIRMATION_PAGE_URI,
+    SUBMISSION_SUMMARY_PAGE_URI
 ];
 
 describe('Authentication Middleware', () => {
@@ -44,24 +52,53 @@ describe('Authentication Middleware', () => {
         it('should not redirect the user to the sign in page if the user is signed in', async () => {
 
             for (const page of protectedPages) {
-                await request(authedApp).get(page)
-                    .expect(200);
+                await request(authedApp).post(page)
+                    .expect(res => expect(res).to.not.equal(302));
             }
 
         });
 
-        it('should continue to the next page without requiring auth if penalty details are valid', async () => {
+        it('should call next if the user is signed in', () => {
 
-            await request(authedApp)
-                .post(PENALTY_DETAILS_PAGE_URI)
-                .send(penaltyIdentifier)
-                .expect(_ => expect(_.header.location).to.include(OTHER_REASON_DISCLAIMER_PAGE_URI));
+            const mockRequest = {
+                session: Maybe.of(session)
+            } as Request;
+            const mockResponse = Substitute.for<Response>();
+            const mockNext = Substitute.for<NextFunction>();
+
+            const authMiddleware = new AuthMiddleware().handler;
+
+            authMiddleware(mockRequest, mockResponse, mockNext);
+
+            //@ts-ignore
+            mockResponse.didNotReceive().redirect(Arg.any());
 
         });
     });
     describe('no session scenario', () => {
+
+        it('should not call next if the user is not signed in', () => {
+
+            const unAuthedSession = createFakeSession([], config.cookieSecret, false);
+
+            const mockRequest = {
+                session: Maybe.of(unAuthedSession)
+            } as Request;
+            const mockResponse = Substitute.for<Response>();
+            const mockNext = Substitute.for<NextFunction>();
+
+            const authMiddleware = new AuthMiddleware().handler;
+
+            authMiddleware(mockRequest, mockResponse, mockNext);
+
+            //@ts-ignore
+            mockResponse.received().redirect(Arg.any());
+
+        });
+
         it('should redirect the user to sign in screen when trying to access protected pages', async () => {
-            const appWithoutSession = createApp()
+
+            const appWithoutSession = createApp();
 
             for (const page of protectedPages) {
                 await request(appWithoutSession).get(page)
