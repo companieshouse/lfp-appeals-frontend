e009bd7806654b6959d45cbc732fdd62aed805d4
Damian Dunajski <ddunajski@companieshouse.gov.uk>
2020-02-27 00:27:40 +0000

    Implement poof of concept version of email sending

--- src/controllers/CheckYourAppealController.ts
+++ src/controllers/CheckYourAppealController.ts
@@ -10,6 +10,11 @@ import { AppealKeys } from '../models/keys/AppealKeys';
 import { BaseAsyncHttpController } from './BaseAsyncHttpController';
 import { HttpResponseMessage } from 'inversify-express-utils/dts/httpResponseMessage';
 
+import * as kafka from 'kafka-node'
+import * as util from 'util'
+
+import * as avro from 'avsc'
+
 @controller(SUBMISSION_SUMMARY_PAGE_URI, SessionMiddleware, AuthMiddleware)
 export class CheckYourAppealController extends BaseAsyncHttpController {
 
@@ -29,7 +34,44 @@ export class CheckYourAppealController extends BaseAsyncHttpController {
     }
 
     @httpPost('')
-    public async handleFormSubmission(): Promise<HttpResponseMessage> {
+    public async handleFormSubmission(req: Request): Promise<HttpResponseMessage> {
+        const client = new kafka.KafkaClient({ kafkaHost: process.env.KAFKA_BROKER_ADDR });
+        const producer = new kafka.Producer(client);
+
+        const type = avro.Type.forSchema({
+            type: 'record',
+            name: 'message',
+            fields: [
+                { name: 'app_id', type: 'string' },
+                { name: 'message_id', type: 'string' },
+                { name: 'message_type', type: 'string' },
+                { name: 'data', type: 'string' },
+                { name: 'email_address', type: 'string' },
+                { name: 'created_at', type: 'string' }
+            ]
+        });
+
+        await util.promisify(producer.send).bind(producer)([{
+            topic: 'email-send',
+            messages: [
+                type.toBuffer({
+                    app_id: 'lfp-appeals-frontend',
+                    message_id: '5afdb299-d0a7-4a8e-a0ba-ed649ea56ad0',
+                    message_type: 'lfp-appeal-submission-confirmation',
+                    data: JSON.stringify({
+                        to: 'demo@ch.gov.uk',
+                        subject: 'Appeal submitted',
+                        companyNumber: '12345678',
+                        userProfile: {
+                            email: 'demo@ch.gov.uk'
+                        }
+                    }),
+                    email_address: 'demo@ch.gov.uk',
+                    created_at: new Date().toISOString()
+                })
+            ]
+        }]);
+
         return this.redirect(CONFIRMATION_PAGE_URI).executeAsync();
     }
 }
