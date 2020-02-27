0fab548a12822318904951f3af3846f048ff1e4b
n-r-b <nblanchette@companieshouse.gov.uk>
2020-02-27 11:36:07 +0000

    test: update signin return url

--- src/middleware/AuthMiddleware.ts
+++ src/middleware/AuthMiddleware.ts
@@ -1,11 +1,9 @@
 import { BaseMiddleware } from 'inversify-express-utils';
 import { NextFunction, Request, RequestHandler, Response } from 'express';
-import { getEnvOrDefault } from '../utils/EnvironmentUtils';
 import { ISignInInfo } from 'ch-node-session-handler/lib/session/model/SessionInterfaces';
 import { SessionKey } from 'ch-node-session-handler/lib/session/keys/SessionKey';
 import { SignInInfoKeys } from 'ch-node-session-handler/lib/session/keys/SignInInfoKeys';
 import { injectable } from 'inversify';
-import { PENALTY_DETAILS_PAGE_URI } from '../utils/Paths';
 import { VerifiedSession } from 'ch-node-session-handler/lib/session/model/Session';
 import { Maybe } from 'ch-node-session-handler';
 
@@ -13,12 +11,14 @@ import { Maybe } from 'ch-node-session-handler';
 export class AuthMiddleware extends BaseMiddleware {
 
     public handler: RequestHandler = (req: Request, res: Response, next: NextFunction): void => {
+
         const signedIn: Maybe<boolean> = req.session
             .chain((session: VerifiedSession) => session.getValue<ISignInInfo>(SessionKey.SignInInfo))
             .map((signInInfo: ISignInInfo) => signInInfo[SignInInfoKeys.SignedIn] === 1);
 
         if (!signedIn.orDefault(false)) {
-            res.redirect(`${getEnvOrDefault('ACCOUNT_URL', '')}/signin?return_to=${PENALTY_DETAILS_PAGE_URI}`);
+
+            res.redirect('/signin?return_to=' + req.originalUrl);
         } else {
             next();
         }
