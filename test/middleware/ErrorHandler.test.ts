3e6a5dfe1a16a9144128d3db451934de04e31b0d
Damian Dunajski <ddunajski@companieshouse.gov.uk>
2020-03-06 15:58:29 +0000

    Add integration test for default handler

--- test/middleware/ErrorHandler.test.ts
+++ test/middleware/ErrorHandler.test.ts
@@ -1,28 +1,49 @@
 import 'reflect-metadata';
 
 import { expect } from 'chai';
-import { NOT_FOUND } from 'http-status-codes';
+import * as express from 'express';
+import { INTERNAL_SERVER_ERROR, NOT_FOUND } from 'http-status-codes';
 import * as request from 'supertest';
 
-import 'app/controllers/index';
-import { notFoundHandler } from 'app/middleware/ErrorHandler';
-
-import { createApp } from 'test/ApplicationFactory';
+import { defaultHandler, notFoundHandler } from 'app/middleware/ErrorHandler';
+import { getExpressAppConfig as configureApplication } from 'app/utils/ConfigLoader';
 
 const pageHeading = 'Sorry, there is a problem with the service';
 const FAKE_PAGE_URI = '/fake-page';
 
+function createApp(): express.Application {
+    const application = express();
+    configureApplication('../..')(application);
+    return application;
+}
+
 describe('Error Handler Middleware', () => {
     it('should render error page if redirected to wrong route', async () => {
-        const app = createApp();
-        app.use(notFoundHandler);
-        await request(app).get(FAKE_PAGE_URI)
-            .expect(response => {
-                expect(response.status).to.be.equal(NOT_FOUND);
-                expect(response.text).to.contain(pageHeading);
-            })
-        }
+            const app = createApp()
+                .use(notFoundHandler);
 
+            await request(app).get(FAKE_PAGE_URI)
+                .expect(response => {
+                    expect(response.status).to.be.equal(NOT_FOUND);
+                    expect(response.text).to.contain(pageHeading);
+                })
+        }
+    );
+
+    it('should render error page when an error was thrown', async () => {
+            const app = createApp()
+                // @ts-ignore
+                .use(FAKE_PAGE_URI, (req: express.Request, res: express.Response, next: express.NextFunction) => {
+                    throw new Error(':(')
+                })
+                .use(defaultHandler);
+
+            await request(app).get(FAKE_PAGE_URI)
+                .expect(response => {
+                    expect(response.status).to.be.equal(INTERNAL_SERVER_ERROR);
+                    expect(response.text).to.contain(pageHeading);
+                })
+        }
     );
 });
 
