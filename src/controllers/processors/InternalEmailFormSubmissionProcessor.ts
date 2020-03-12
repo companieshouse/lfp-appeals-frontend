3902d9d9dc2b2fbeeeeb79a6eb6a74be1211e134
Damian Dunajski <ddunajski@companieshouse.gov.uk>
2020-03-12 11:36:43 +0000

    Add unit tests for processors

--- src/controllers/processors/InternalEmailFormSubmissionProcessor.ts
+++ src/controllers/processors/InternalEmailFormSubmissionProcessor.ts
@@ -1,5 +1,3 @@
-// tslint:disable-next-line:max-classes-per-file
-import { Maybe } from 'ch-node-session-handler';
 import { SessionKey } from 'ch-node-session-handler/lib/session/keys/SessionKey';
 import { SignInInfoKeys } from 'ch-node-session-handler/lib/session/keys/SignInInfoKeys';
 import { ISignInInfo, IUserProfile } from 'ch-node-session-handler/lib/session/model/SessionInterfaces';
@@ -14,6 +12,29 @@ import { EmailService } from 'app/modules/email-publisher/EmailService';
 import { getEnvOrDefault } from 'app/utils/EnvironmentUtils';
 import { findRegionByCompanyNumber } from 'app/utils/RegionLookup';
 
+function buildEmail(userProfile: IUserProfile, appeal: Appeal): Email {
+    const region = findRegionByCompanyNumber(appeal.penaltyIdentifier.companyNumber);
+    return {
+        to: getEnvOrDefault(`${region}_TEAM_EMAIL`),
+        subject: `Appeal submitted - ${appeal.penaltyIdentifier.companyNumber}`,
+        body: {
+            templateName: 'lfp-appeal-submission-internal',
+            templateData: {
+                companyNumber: appeal.penaltyIdentifier.companyNumber,
+                userProfile: {
+                    email: userProfile.email
+                },
+                reasons: {
+                    other: {
+                        title: appeal.reasons.other.title,
+                        description: appeal.reasons.other.description
+                    }
+                }
+            }
+        }
+    };
+}
+
 @provide(InternalEmailFormSubmissionProcessor)
 export class InternalEmailFormSubmissionProcessor implements FormSubmissionProcessor {
     constructor(@inject(EmailService) private readonly emailService: EmailService) {}
@@ -22,36 +43,13 @@ export class InternalEmailFormSubmissionProcessor implements FormSubmissionProce
         const userProfile = req.session
             .chain(_ => _.getValue<ISignInInfo>(SessionKey.SignInInfo))
             .map(info => info[SignInInfoKeys.UserProfile])
-            .orDefault({});
+            .unsafeCoerce() as IUserProfile;
 
         const appeal = req.session
             .chain(_ => _.getExtraData())
-            .chain(data => Maybe.fromNullable(data[APPEALS_KEY]))
-            .orDefault({});
+            .map(data => data[APPEALS_KEY] as Appeal)
+            .unsafeCoerce();
 
-        await this.emailService.send(this.buildEmail(userProfile as IUserProfile, appeal));
-    }
-
-    private buildEmail(userProfile: IUserProfile, appeal: Appeal): Email {
-        const region = findRegionByCompanyNumber(appeal.penaltyIdentifier.companyNumber);
-        return {
-            to: getEnvOrDefault(`${region}_TEAM_EMAIL`),
-            subject: `Appeal submitted - ${appeal.penaltyIdentifier.companyNumber}`,
-            body: {
-                templateName: 'lfp-appeal-submission-internal',
-                templateData: {
-                    companyNumber: appeal.penaltyIdentifier.companyNumber,
-                    userProfile: {
-                        email: userProfile.email
-                    },
-                    reasons: {
-                        other: {
-                            title: appeal.reasons.other.title,
-                            description: appeal.reasons.other.description
-                        }
-                    }
-                }
-            }
-        };
+        await this.emailService.send(buildEmail(userProfile as IUserProfile, appeal));
     }
 }
--- src/controllers/processors/UserEmailFormSubmissionProcessor.ts
+++ src/controllers/processors/UserEmailFormSubmissionProcessor.ts
@@ -1,4 +1,3 @@
-import { Maybe } from 'ch-node-session-handler';
 import { SessionKey } from 'ch-node-session-handler/lib/session/keys/SessionKey';
 import { SignInInfoKeys } from 'ch-node-session-handler/lib/session/keys/SignInInfoKeys';
 import { ISignInInfo, IUserProfile } from 'ch-node-session-handler/lib/session/model/SessionInterfaces';
@@ -11,6 +10,22 @@ import { Appeal, APPEALS_KEY } from 'app/models/Appeal';
 import { Email } from 'app/modules/email-publisher/Email';
 import { EmailService } from 'app/modules/email-publisher/EmailService';
 
+function buildEmail(userProfile: IUserProfile, appeal: Appeal): Email {
+    return {
+        to: userProfile.email as string,
+        subject: `Confirmation of your appeal - ${appeal.penaltyIdentifier.companyNumber} - Companies House`,
+        body: {
+            templateName: 'lfp-appeal-submission-confirmation',
+            templateData: {
+                companyNumber: appeal.penaltyIdentifier.companyNumber,
+                userProfile: {
+                    email: userProfile.email
+                }
+            }
+        }
+    };
+}
+
 @provide(UserEmailFormSubmissionProcessor)
 export class UserEmailFormSubmissionProcessor implements FormSubmissionProcessor {
     constructor(@inject(EmailService) private readonly emailService: EmailService) {}
@@ -19,29 +34,13 @@ export class UserEmailFormSubmissionProcessor implements FormSubmissionProcessor
         const userProfile = req.session
             .chain(_ => _.getValue<ISignInInfo>(SessionKey.SignInInfo))
             .map(info => info[SignInInfoKeys.UserProfile])
-            .orDefault({});
+            .unsafeCoerce() as IUserProfile;
 
-        const appealsData = req.session
+        const appeal = req.session
             .chain(_ => _.getExtraData())
-            .chain(data => Maybe.fromNullable(data[APPEALS_KEY]))
-            .orDefault({});
+            .map(data => data[APPEALS_KEY] as Appeal)
+            .unsafeCoerce();
 
-        await this.emailService.send(this.buildEmail(userProfile as IUserProfile, appealsData));
-    }
-
-    private buildEmail(userProfile: IUserProfile, appeal: Appeal): Email {
-        return {
-            to: userProfile?.email as string,
-            subject: `Confirmation of your appeal - ${appeal.penaltyIdentifier.companyNumber} - Companies House`,
-            body: {
-                templateName: 'lfp-appeal-submission-confirmation',
-                templateData: {
-                    companyNumber: appeal.penaltyIdentifier.companyNumber,
-                    userProfile: {
-                        email: userProfile?.email
-                    }
-                }
-            }
-        };
+        await this.emailService.send(buildEmail(userProfile, appeal));
     }
 }
