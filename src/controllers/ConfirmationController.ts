cfeb6e113492f7dfcaae8e83b8e6ea619455c8a2
akaJSaunders <jsaunders1@companieshouse.gov.uk>
2020-03-13 16:04:37 +0000

    fix: add checks on confirmation page

--- src/controllers/ConfirmationController.ts
+++ src/controllers/ConfirmationController.ts
@@ -8,13 +8,15 @@ import { controller, httpGet } from 'inversify-express-utils';
 import { BaseAsyncHttpController } from 'app/controllers/BaseAsyncHttpController';
 import { AuthMiddleware } from 'app/middleware/AuthMiddleware';
 import { ApplicationData, APPEALS_KEY } from 'app/models/Appeal';
-import { CONFIRMATION_PAGE_URI } from 'app/utils/Paths';
+import { CONFIRMATION_PAGE_URI, PENALTY_DETAILS_PAGE_URI } from 'app/utils/Paths';
 
 @controller(CONFIRMATION_PAGE_URI, SessionMiddleware, AuthMiddleware)
 export class ConfirmationController extends BaseAsyncHttpController {
 
     @httpGet('')
     public async getConfirmationView(req: Request): Promise<void> {
+
+        this.checkNavigationPermissions()
 
         const companyNumber = req.session
             .chain(_ => _.getExtraData())
@@ -31,5 +33,31 @@ export class ConfirmationController extends BaseAsyncHttpController {
 
         return this.render('confirmation', { companyNumber, userEmail });
 
+    }
+
+    private checkNavigationPermissions(): void{
+        const session = this.httpContext.request.session.unsafeCoerce();
+        const applicationData = session.getExtraData()
+            .map<ApplicationData>(data => data[APPEALS_KEY])
+            .orDefault({
+                navigation: {}
+            } as ApplicationData);
+
+        console.log(applicationData);
+
+        if(applicationData.navigation.permissions === undefined) {
+            console.log('Start of journey');
+            if(this.httpContext.request.url !== PENALTY_DETAILS_PAGE_URI){
+                return this.httpContext.response.redirect(PENALTY_DETAILS_PAGE_URI);
+            }
+        } else {
+            const permissions = applicationData.navigation.permissions;
+            if (!applicationData.navigation.permissions.includes(this.httpContext.request.url)) {
+                console.log('Redirecting, No pass to enter: ', this.httpContext.request.url);
+                return this.httpContext.response.redirect(permissions[permissions.length - 1]);
+            }
+        }
+
+        console.log('welcome to: ', this.httpContext.request.url);
     }
 }
