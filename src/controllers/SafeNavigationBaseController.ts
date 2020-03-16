d2bd97db40f440827fad5f0655232238c1590726
Juliano Saunders <julianoasaunders@gmail.com>
2020-03-16 15:35:07 +0000

    fix: allow change button to redirect (#99)

--- src/controllers/SafeNavigationBaseController.ts
+++ src/controllers/SafeNavigationBaseController.ts
@@ -70,13 +70,15 @@ export abstract class SafeNavigationBaseController<FORM> extends BaseController<
             } as ApplicationData);
 
         if (applicationData.navigation.permissions === undefined) {
-            if (this.httpContext.request.url !== PENALTY_DETAILS_PAGE_URI) {
+            if (this.httpContext.request.path !== PENALTY_DETAILS_PAGE_URI) {
                 return this.httpContext.response.redirect(PENALTY_DETAILS_PAGE_URI);
             }
         } else {
             const permissions = applicationData.navigation.permissions;
-            if (!applicationData.navigation.permissions.includes(this.httpContext.request.url)) {
-                return this.httpContext.response.redirect(permissions[permissions.length - 1]);
+            if (!applicationData.navigation.permissions.includes(this.httpContext.request.path)) {
+                if(this.httpContext.request.path !== PENALTY_DETAILS_PAGE_URI){
+                    return this.httpContext.response.redirect(permissions[permissions.length - 1]);
+                }
             }
         }
 
