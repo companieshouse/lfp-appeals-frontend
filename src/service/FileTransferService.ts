0b722b6f9fbcf4798c9a890c0b07036943781148
Juliano Saunders <julianoasaunders@gmail.com>
2020-03-31 17:55:48 +0100

    feat: add file transfer service

---
+++ src/service/FileTransferService.ts
@@ -0,0 +1,51 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import FormData from 'form-data';
+import { CREATED, UNSUPPORTED_MEDIA_TYPE } from 'http-status-codes';
+
+import { loggerInstance } from 'app/middleware/Logger';
+
+export class FileTransferService {
+
+    constructor(private readonly url: string, private readonly key: string) {
+        this.url = url;
+        this.key = key;
+    }
+
+    public async upload(file: Buffer, fileName: string): Promise<string> {
+
+        if (file == null) {
+            throw new Error('File is missing');
+        }
+
+        if (fileName == null) {
+            throw new Error('File name is missing');
+        }
+
+        const data = new FormData();
+        data.append('upload', file, fileName);
+
+        const config: AxiosRequestConfig = {
+            headers: {
+                'x-api-key': this.key,
+                ...data.getHeaders()
+            }
+        };
+
+        loggerInstance()
+            .debug(`Making a POST request to ${this.url}`);
+
+        return await axios
+            .post(this.url, data, config)
+            .then((response: AxiosResponse) => {
+                if (response.status === CREATED && response.data.id) {
+                    return response.data.id;
+                }
+            }).catch((err) => {
+                if (err.code === UNSUPPORTED_MEDIA_TYPE) {
+                    throw new Error('Unsupported file type')
+                } else {
+                    throw new Error(err.message)
+                }
+            });
+    }
+}
---
+++ test/service/FileTransferService.test.ts
@@ -0,0 +1,82 @@
+import { expect } from 'chai';
+import { CREATED, UNSUPPORTED_MEDIA_TYPE } from 'http-status-codes';
+import nock = require('nock');
+
+import { FileTransferService } from 'app/service/FileTransferService'
+
+describe('FileTransferService', () => {
+
+    const KEY: string = 'mock-key';
+    const HOST: string = 'http://localhost';
+    const URI: string = '/dev/files';
+    const fileTransferService = new FileTransferService(HOST + URI , KEY);
+
+    describe('upload file', () => {
+
+        it('should throw an error when evidence not defined', () => {
+
+            [undefined, null].forEach(async evidence => {
+                try {
+                    await fileTransferService.upload(evidence!, 'filename')
+                } catch (err) {
+                    expect(err).to.be.instanceOf(Error)
+                        .and.to.haveOwnProperty('message').equal('File is missing')
+                }
+            })
+        });
+
+        it('should throw an error when file name not defined', () => {
+
+            [undefined, null].forEach(async filename => {
+                try {
+                    await fileTransferService.upload(Buffer.from('This is a test'), filename!)
+                } catch (err) {
+                    expect(err).to.be.instanceOf(Error)
+                        .and.to.haveOwnProperty('message').equal('File name is missing')
+                }
+            })
+        });
+
+        it('should return file ID when supported media uploaded', async () => {
+
+            const evidenceID: string = 'mock-id';
+
+            nock(HOST)
+                .post(URI,
+                    new RegExp(`form-data; name="upload"; filename="test.supported"`,'m'),
+                    {
+                        reqheaders: {
+                            'x-api-key': KEY
+                        },
+                    }
+                )
+                .reply(CREATED, { id: evidenceID });
+
+            const response = await fileTransferService.upload(Buffer.from('This is a test'), 'test.supported');
+            expect(response).to.equal(evidenceID);
+        });
+
+        it('should throw error when unsupported media uploaded', async () => {
+
+            nock(HOST)
+                .post(URI,
+                    new RegExp(`form-data; name="upload"; filename="test.not_supported"`,'m'),
+                    {
+                        reqheaders: {
+                            'x-api-key': KEY
+                        },
+                    }
+                )
+                .replyWithError({
+                    message: { message: 'unsupported file type' },
+                    code: UNSUPPORTED_MEDIA_TYPE,
+                });
+
+            try {
+                await fileTransferService.upload(Buffer.from('This is a test'), 'test.not_supported');
+            } catch(err) {
+                expect(err.message).to.contain('Unsupported file type');
+            }
+        });
+    });
+});
