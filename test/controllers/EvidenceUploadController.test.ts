ddbb22572f17b2ba338eb826ec9e3b5983c685d5
Damian Dunajski <ddunajski@companieshouse.gov.uk>
2020-04-02 23:31:27 +0100

    Cleanup evidence controller test

--- test/controllers/EvidenceUploadController.test.ts
+++ test/controllers/EvidenceUploadController.test.ts
@@ -2,13 +2,14 @@ import 'reflect-metadata'
 
 import { expect } from 'chai';
 import { OK } from 'http-status-codes';
-import { afterEach, beforeEach } from 'mocha';
 import request from 'supertest';
 import supertest from 'supertest';
 
 import 'app/controllers/EvidenceUploadController'
 import { Appeal } from 'app/models/Appeal';
 import { ApplicationData, APPLICATION_DATA_KEY } from 'app/models/ApplicationData';
+import { Attachment } from 'app/models/Attachment';
+import { Navigation } from 'app/models/Navigation';
 import { EVIDENCE_UPLOAD_PAGE_URI } from 'app/utils/Paths';
 
 import { createApp, getDefaultConfig } from 'test/ApplicationFactory';
@@ -16,12 +17,11 @@ import { createFakeSession } from 'test/utils/session/FakeSessionFactory';
 
 const config = getDefaultConfig();
 
-const navigation =
-    {
-        permissions: [EVIDENCE_UPLOAD_PAGE_URI]
-    };
+const navigation: Navigation = {
+    permissions: [EVIDENCE_UPLOAD_PAGE_URI]
+};
 
-const appeal = {
+const appeal: Appeal = {
     penaltyIdentifier: {
         companyNumber: '00345567',
         penaltyReference: 'A00000001',
@@ -32,39 +32,25 @@ const appeal = {
             description: 'they are legit',
             attachments: [
                 {
-                    name: 'somefile.jpeg'
-                },
+                    name: 'some-file.jpeg'
+                } as Attachment,
                 {
-                    name: 'anotherfile.jpeg'
-                }
+                    name: 'another-file.jpeg'
+                } as Attachment
             ]
         }
     }
-} as Appeal;
-
-let initialFileTransferFlag: string | undefined;
+};
 
 describe('EvidenceUploadController', () => {
-
-    beforeEach(() => {
-        initialFileTransferFlag = process.env.FILE_TRANSFER_FEATURE;
-    });
-
-    afterEach(() => {
-        process.env.FILE_TRANSFER_FEATURE = initialFileTransferFlag;
-    });
 
     describe('GET request', () => {
         it('should return 200 when trying to access the evidence-upload page', async () => {
 
-            process.env.FILE_TRANSFER_FEATURE = '1';
-
-            const applicationData = {
-                navigation
-            } as ApplicationData;
+            const applicationData = { navigation } as ApplicationData;
 
             const session = createFakeSession([], config.cookieSecret, true)
-                .saveExtraData('appeals', applicationData);
+                .saveExtraData(APPLICATION_DATA_KEY, applicationData);
             const app = createApp(session);
 
             await request(app).get(EVIDENCE_UPLOAD_PAGE_URI)
@@ -76,23 +62,17 @@ describe('EvidenceUploadController', () => {
 
         it('should return 200 when trying to access page with session data', async () => {
 
-            process.env.FILE_TRANSFER_FEATURE = '1';
-
-            const applicationData = {
-                appeal,
-                navigation
-            } as ApplicationData;
+            const applicationData: ApplicationData = { appeal, navigation };
 
             const session = createFakeSession([], config.cookieSecret, true)
                 .saveExtraData(APPLICATION_DATA_KEY, applicationData);
-
             const app = createApp(session);
 
             await request(app).get(EVIDENCE_UPLOAD_PAGE_URI)
                 .expect((response: supertest.Response) => {
                     expect(response.status).to.be.equal(OK);
-                    expect(response.text).to.contain('somefile.jpeg')
-                        .and.to.contain('anotherfile.jpeg');
+                    expect(response.text).to.contain('some-file.jpeg')
+                        .and.to.contain('another-file.jpeg');
                 });
         });
     });
