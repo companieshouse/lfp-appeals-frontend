b7470ed982b84fa8c74952f9af7d1f506027b460
Damian Dunajski <ddunajski@companieshouse.gov.uk>
2020-04-02 22:31:45 +0100

    Reimplement logging in feature toggle middleware

--- src/middleware/FileTransferFeatureMiddleware.ts
+++ src/middleware/FileTransferFeatureMiddleware.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, RequestHandler, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { provide } from 'inversify-binding-decorators';
 import { BaseMiddleware } from 'inversify-express-utils';
 
@@ -9,13 +9,12 @@ import { ENTRY_PAGE_URI } from 'app/utils/Paths';
 @provide(FileTransferFeatureMiddleware)
 export class FileTransferFeatureMiddleware extends BaseMiddleware {
 
-    public handler: RequestHandler = (req: Request, res: Response, next: NextFunction): void => {
-
-        loggerInstance().info(`${FileTransferFeatureMiddleware.name} - handler: request url is ` + req.url);
+    public handler(req: Request, res: Response, next: NextFunction): void {
 
         if (getEnv('FILE_TRANSFER_FEATURE') === '1') {
             next();
         } else {
+            loggerInstance().info(`File transfer feature is disabled - request to ${req.url} got redirected to ${ENTRY_PAGE_URI}`);
             res.redirect(ENTRY_PAGE_URI);
         }
     }
