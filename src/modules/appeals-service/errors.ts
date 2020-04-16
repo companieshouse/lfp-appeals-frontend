acfc61ef8e6c45d5e5af0c16e092d2f240accc08
franciscog-kainos <54070318+franciscog-kainos@users.noreply.github.com>
2020-04-16 14:16:23 +0100

    feat: added middleware to load appeal from mongo (#152)

--- src/modules/appeals-service/errors.ts
+++ src/modules/appeals-service/errors.ts
@@ -1,8 +1,9 @@
 // tslint:disable: max-classes-per-file
-export class AppealNotFoundError extends Error {}
+export class AppealServiceError extends Error { }
 
-export class AppealServiceError extends Error {}
+export class AppealNotFoundError extends Error { }
 
-export class AppealUnauthorisedError extends Error {}
+export class AppealUnauthorisedError extends Error { }
 
-export class AppealUnprocessableEntityError extends Error {}
+export class AppealUnprocessableEntityError extends Error { }
+
