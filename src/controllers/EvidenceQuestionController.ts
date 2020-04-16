bf608e30c7703a56542cc149648c6a119c05129d
Juliano Saunders <julianoasaunders@gmail.com>
2020-04-16 09:52:15 +0100

    Feature: add transition to and from evidence question page

--- src/controllers/EvidenceQuestionController.ts
+++ src/controllers/EvidenceQuestionController.ts
@@ -1,27 +1,30 @@
 import Joi from '@hapi/joi';
 import { SessionMiddleware } from 'ch-node-session-handler';
+import { Request } from 'express';
 import { controller } from 'inversify-express-utils';
 
-import { BaseController } from 'app/controllers/BaseController';
+import { SafeNavigationBaseController } from 'app/controllers/SafeNavigationBaseController';
 import { FormValidator } from 'app/controllers/validators/FormValidator';
 import { AuthMiddleware } from 'app/middleware/AuthMiddleware';
 import { FileTransferFeatureMiddleware } from 'app/middleware/FileTransferFeatureMiddleware';
 import { Attachment } from 'app/models/Attachment';
+import { YesNo } from 'app/models/fields/YesNo';
 import { createSchema } from 'app/models/fields/YesNo.schema';
-import {
-    EVIDENCE_QUESTION_URI,
-    OTHER_REASON_PAGE_URI
-} from 'app/utils/Paths';
+import { CHECK_YOUR_APPEAL_PAGE_URI, EVIDENCE_QUESTION_URI, EVIDENCE_UPLOAD_PAGE_URI } from 'app/utils/Paths';
 import { Navigation } from 'app/utils/navigation/navigation';
 
 const template = 'evidence-question';
 
 const navigation: Navigation = {
     previous(): string {
-        return OTHER_REASON_PAGE_URI;
-    },
-    next(): string {
         return EVIDENCE_QUESTION_URI;
+    },
+    next(request: Request): string {
+        if (request.body.evidence === YesNo.yes) {
+            return EVIDENCE_UPLOAD_PAGE_URI;
+        } else {
+            return CHECK_YOUR_APPEAL_PAGE_URI;
+        }
     }
 };
 
@@ -30,7 +33,7 @@ const schema: Joi.AnySchema = Joi.object({
 }).unknown(true);
 
 @controller(EVIDENCE_QUESTION_URI, SessionMiddleware, AuthMiddleware, FileTransferFeatureMiddleware)
-export class EvidenceQuestionController extends BaseController<Attachment> {
+export class EvidenceQuestionController extends SafeNavigationBaseController<Attachment> {
     constructor() {
         super(template, navigation, new FormValidator(schema));
     }
