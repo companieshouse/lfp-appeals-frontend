49d99532e49443e0d9165e94ab97c24a98cf75df
Damian Dunajski <damdun@gmail.com>
2020-04-22 11:38:59 +0100

    Make profile optional and add tests

--- src/utils/ConfigLoader.ts
+++ src/utils/ConfigLoader.ts
@@ -2,15 +2,10 @@ import * as dotenv from 'dotenv';
 
 const DEFAULT_ENV_FILE = `${__dirname}/../../.env`;
 
-const checkFileExists = (config: dotenv.DotenvConfigOutput) => {
-    if (config.error) throw config.error;
-    else return config;
-};
-
 export const loadEnvironmentVariablesFromFiles = () => {
     dotenv.config({ path: DEFAULT_ENV_FILE });
     if (process.env.NODE_ENV) {
         const envFilePath = `${__dirname}/../../.env.${process.env.NODE_ENV}`;
-        checkFileExists(dotenv.config({ path: envFilePath }));
+        dotenv.config({ path: envFilePath });
     }
 };
