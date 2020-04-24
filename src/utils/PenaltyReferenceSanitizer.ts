c5ec72368ad77764398ab4a7463ad1d3c36d956a
akaJSaunders <jsaunders1@companieshouse.gov.uk>
2020-04-24 11:24:06 +0100

    refactor sanitizer

--- src/utils/PenaltyReferenceSanitizer.ts
+++ src/utils/PenaltyReferenceSanitizer.ts
@@ -4,7 +4,7 @@ export const sanitizePenalty = (penaltyReference: string): string => {
 
     penaltyReference = penaltyReference.toUpperCase();
     const prefix = penaltyReference.substring(0,6);
-    const cleanCompanyNumber = sanitizeCompany(penaltyReference.substring(6, penaltyReference.length));
-    return prefix + cleanCompanyNumber;
+    const companyNumber = sanitizeCompany(penaltyReference.substring(6, penaltyReference.length));
+    return prefix + companyNumber;
 
 };
