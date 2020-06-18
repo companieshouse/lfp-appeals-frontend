073037c6ee9a498f87ba33989011c11f50769677
Damian Dunajski <damdun@gmail.com>
2020-06-18 13:54:12 +0100

    feat: add accessibility statement

--- src/utils/Paths.ts
+++ src/utils/Paths.ts
@@ -9,6 +9,7 @@ export const HEALTH_CHECK_URI = `${ROOT_URI}/healthcheck`;
 
 // Pages
 export const ENTRY_PAGE_URI = `${ROOT_URI}/start`;
+export const ACCESSIBILITY_STATEMENT_URI = `${ROOT_URI}/accessibility-statement`;
 export const PENALTY_DETAILS_PAGE_URI = `${ROOT_URI}/penalty-reference`;
 export const SELECT_THE_PENALTY_PAGE_URI = `${ROOT_URI}/select-the-penalty`;
 export const REVIEW_PENALTY_PAGE_URI =`${ROOT_URI}/review-penalty`;
