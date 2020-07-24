463a5bafad4753fdbf62190e495bbab46eb2c6fd
n-r-b <nblanchette@companieshouse.gov.uk>
2020-07-24 10:53:41 +0100

    refact: update applied company auth integration test

--- test/middleware/AppliedCompanyAuthMiddleware.test.ts
+++ test/middleware/AppliedCompanyAuthMiddleware.test.ts
@@ -5,14 +5,30 @@ import { createApp } from '../ApplicationFactory';
 
 import { Appeal } from 'app/models/Appeal';
 import { ApplicationData } from 'app/models/ApplicationData';
-import { REVIEW_PENALTY_PAGE_URI, SELECT_THE_PENALTY_PAGE_URI } from 'app/utils/Paths';
+import {
+    CHECK_YOUR_APPEAL_PAGE_URI,
+    CONFIRMATION_PAGE_URI,
+    EVIDENCE_QUESTION_URI,
+    EVIDENCE_REMOVAL_PAGE_URI,
+    EVIDENCE_UPLOAD_PAGE_URI, OTHER_REASON_DISCLAIMER_PAGE_URI,
+    OTHER_REASON_PAGE_URI,
+    REVIEW_PENALTY_PAGE_URI,
+    SELECT_THE_PENALTY_PAGE_URI
+} from 'app/utils/Paths';
 
 
 describe('All pages after the Penalty Details page:', () => {
 
     const pageList = [
         { name: 'Select Penalty', uri: SELECT_THE_PENALTY_PAGE_URI},
-        { name: 'Review Penalty', uri: REVIEW_PENALTY_PAGE_URI}
+        { name: 'Review Penalty', uri: REVIEW_PENALTY_PAGE_URI},
+        { name: 'Other Reason Disclaimer', uri: OTHER_REASON_DISCLAIMER_PAGE_URI},
+        { name: 'Other Reason', uri: OTHER_REASON_PAGE_URI},
+        { name: 'Evidence Upload', uri: EVIDENCE_UPLOAD_PAGE_URI},
+        { name: 'Evidence Question', uri: EVIDENCE_QUESTION_URI},
+        { name: 'Evidence Removal', uri: EVIDENCE_REMOVAL_PAGE_URI},
+        { name: 'Check Your Appeal', uri: CHECK_YOUR_APPEAL_PAGE_URI},
+        { name: 'Confirmation', uri: CONFIRMATION_PAGE_URI}
     ];
 
     pageList.forEach((page) => {
