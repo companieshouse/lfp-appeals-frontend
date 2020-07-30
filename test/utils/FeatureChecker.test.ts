d1aeb054a4c74342eff37b12d356e8f4eadafd24
Natalie Blanchette <16392604+n-r-b@users.noreply.github.com>
2020-07-30 12:33:01 +0100

    refct: create reusable feature toggle middleware (#258)

--- test/utils/FeatureChecker.test.ts
+++ test/utils/FeatureChecker.test.ts
@@ -7,19 +7,19 @@ describe('Feature checker', () => {
     const feature: Feature = 'SUPER_POWER' as Feature;
 
     it ('should return true when feature is enabled', () => {
-        process.env[`${feature}_FEATURE`] = '1';
+        process.env[`${feature}_FEATURE_ENABLED`] = '1';
         // tslint:disable-next-line: no-unused-expression
         expect(isFeatureEnabled(feature)).to.be.true;
     });
 
     it ('should return false when feature is disabled', () => {
-        process.env[`${feature}_FEATURE`] = '0';
+        process.env[`${feature}_FEATURE_ENABLED`] = '0';
         // tslint:disable-next-line: no-unused-expression
         expect(isFeatureEnabled(feature)).to.be.false;
     });
 
     it ('should return false when feature flag is not configured', () => {
-        delete process.env[`${feature}_FEATURE`];
+        delete process.env[`${feature}_FEATURE_ENABLED`];
 
         // tslint:disable-next-line: no-unused-expression
         expect(isFeatureEnabled(feature)).to.be.false;
