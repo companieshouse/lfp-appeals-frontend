74a4674a5fa49ce96ed11aa4e32c2d3cda5e162d
Rhys Barrett <rbarrett@companieshouse.gov.uk>
2020-07-31 12:24:20 +0100

    Graceful handling of missing NODE_ENV variable

--- src/utils/ConfigLoader.ts
+++ src/utils/ConfigLoader.ts
@@ -1,9 +1,11 @@
 import * as dotenv from 'dotenv';
 
-import { getEnvOrThrow } from 'app/utils/EnvironmentUtils';
+import { getEnv } from 'app/utils/EnvironmentUtils';
 
 export const loadEnvironmentVariablesFromFiles = () => {
-    const env = getEnvOrThrow('NODE_ENV');
-    const envFilePath = `${__dirname}/../../.env.${env}`;
-    dotenv.config({ path: envFilePath });
+    const env = getEnv('NODE_ENV');
+    if (env) {
+        const envFilePath = `${__dirname}/../../.env.${env}`;
+        dotenv.config({ path: envFilePath });
+    }
 };
