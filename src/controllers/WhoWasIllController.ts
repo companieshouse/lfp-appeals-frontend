40974a49420c560d8121cbfad41759ab2702fe42
Rhys Barrett <rbarrett@companieshouse.gov.uk>
2020-07-31 12:19:13 +0100

    Add test

--- src/controllers/WhoWasIllController.ts
+++ src/controllers/WhoWasIllController.ts
@@ -2,11 +2,13 @@ import { SessionMiddleware } from 'ch-node-session-handler';
 import { controller, httpGet } from 'inversify-express-utils';
 
 import { BaseAsyncHttpController } from 'app/controllers/BaseAsyncHttpController';
+import { FeatureToggleMiddleware } from 'app/middleware/FeatureToggleMiddleware';
+import { Feature } from 'app/utils/Feature';
 import { WHO_WAS_ILL_PAGE_URI } from 'app/utils/Paths';
 
 const template = 'illness/who-was-ill';
 
-@controller(WHO_WAS_ILL_PAGE_URI, SessionMiddleware)
+@controller(WHO_WAS_ILL_PAGE_URI, FeatureToggleMiddleware(Feature.ILLNESS_REASON), SessionMiddleware)
 export class WhoWasIllController extends BaseAsyncHttpController {
 
     @httpGet('')
