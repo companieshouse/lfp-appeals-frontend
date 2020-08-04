7a56f0eb1ff92e8f2d58db7e21e37e9567a8c944
Rhys Barrett <rbarrett@companieshouse.gov.uk>
2020-08-04 10:11:40 +0100

    WIP: Choose Reason Transitions

--- src/controllers/ChooseAppealReasonController.ts
+++ src/controllers/ChooseAppealReasonController.ts
@@ -1,23 +1,34 @@
 import { SessionMiddleware } from 'ch-node-session-handler';
+import { request } from 'express';
 import { controller } from 'inversify-express-utils';
-import { BaseController } from './BaseController';
-import { FormValidator } from './validators/FormValidator';
 
+import { BaseController } from 'app/controllers/BaseController';
+import { FormValidator } from 'app/controllers/validators/FormValidator';
 import { AuthMiddleware } from 'app/middleware/AuthMiddleware';
 import { FeatureToggleMiddleware } from 'app/middleware/FeatureToggleMiddleware';
+import { Reason } from 'app/models/fields/Reason';
 import { schema } from 'app/models/fields/Reason.schema';
 import { Feature } from 'app/utils/Feature';
-import { CHOOSE_REASON_PAGE_URI } from 'app/utils/Paths';
+import {
+    CHOOSE_REASON_PAGE_URI,
+    ILL_PERSON_PAGE_URI,
+    OTHER_REASON_DISCLAIMER_PAGE_URI,
+    REVIEW_PENALTY_PAGE_URI } from 'app/utils/Paths';
 import { Navigation } from 'app/utils/navigation/navigation';
 
 const template = 'choose-appeal-reason';
 
 const navigation: Navigation = {
     previous(): string {
-        return '';
+        return REVIEW_PENALTY_PAGE_URI;
     },
     next(): string {
-        return '';
+        switch(request.body.reason) {
+            case Reason.illness:
+                return ILL_PERSON_PAGE_URI;
+            default:
+                return OTHER_REASON_DISCLAIMER_PAGE_URI;
+        }
     },
     actions: (_: boolean) => {
         return {
