de76bfa212e32bef243f2d784e151495fa527528
Rhys Barrett <43062389+rhysbarrett@users.noreply.github.com>
2020-08-04 12:44:10 +0100

    fix: Off-By-One bug in Nunjucks Date filter (#264)
    Update to the custom date filter for Nunjucks, which expected a month value between 0-11 instead of 1-12 (due to previous use of Date type instead of string). Added unit test cases to cover these scenarios.

--- test/modules/nunjucks/DateFilter.test.ts
+++ test/modules/nunjucks/DateFilter.test.ts
@@ -4,10 +4,16 @@ import { dateFilter } from 'app/modules/nunjucks/DateFilter';
 
 describe('Date Filter', () => {
     it('should reformat a correctly-formatted datestring into the format dd Month yyyy', () => {
-        const testInput = '2020-05-01';
-        const result = dateFilter(testInput);
+        const testCases = [
+            { input: '1990-01-21', output: '21 January 1990' },
+            { input: '2005-05-13', output: '13 May 2005' },
+            { input: '2015-12-31', output: '31 December 2015' }
+        ];
 
-        expect(result).to.equal('1 May 2020');
+        testCases.forEach(testCase => {
+            const result = dateFilter(testCase.input);
+            expect(result).to.equal(testCase.output);
+        });
     });
 
     it('should throw an error if a date component is missing', () => {
@@ -17,7 +23,7 @@ describe('Date Filter', () => {
 
     it('should throw an error if a date component is invalid', () => {
         const testInput = '2020-20-01';
-        expect(() => dateFilter(testInput)).to.throw('Input should be formatted as yyyy-MM-dd');
+        expect(() => dateFilter(testInput)).to.throw('Input contains invalid month: 2020-20-01');
     });
 
     it('should throw an error if the input format is invalid', () => {
