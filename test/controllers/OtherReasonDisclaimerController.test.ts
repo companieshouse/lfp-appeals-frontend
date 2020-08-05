1936ed56ffea19c316ee973a7fa256d78cc3308f
Rhys Barrett <rbarrett@companieshouse.gov.uk>
2020-08-05 14:34:21 +0100

    Revert changes to OtherReasonDisclaimer test

--- test/controllers/OtherReasonDisclaimerController.test.ts
+++ test/controllers/OtherReasonDisclaimerController.test.ts
@@ -6,37 +6,34 @@ import request from 'supertest';
 
 import 'app/controllers/OtherReasonDisclaimerController';
 import { Appeal } from 'app/models/Appeal';
-import { ApplicationData } from 'app/models/ApplicationData';
 import { OTHER_REASON_DISCLAIMER_PAGE_URI, OTHER_REASON_PAGE_URI } from 'app/utils/Paths';
 
 import { createApp } from 'test/ApplicationFactory';
 
 describe('OtherReasonDisclaimerController', () => {
 
-    function generateApplicationData(): ApplicationData {
-        return {
-            appeal: {
-                penaltyIdentifier: {
-                    companyNumber: 'NI000000',
-                    penaltyReference: 'A00000001'
-                },
-            } as Appeal,
-            navigation: {
-                permissions: [OTHER_REASON_DISCLAIMER_PAGE_URI]
-            }
-        };
-    }
+    const applicationData = {
+        appeal: {
+            penaltyIdentifier: {
+                companyNumber: 'NI000000',
+                penaltyReference: 'A00000001'
+            },
+        } as Appeal,
+        navigation: {
+            permissions: [OTHER_REASON_DISCLAIMER_PAGE_URI]
+        }
+    };
 
     describe('GET request', () => {
         it('should return 200 when trying to access the other-reason-entry page', async () => {
-            const app = createApp(generateApplicationData());
+            const app = createApp(applicationData);
             await request(app).get(OTHER_REASON_DISCLAIMER_PAGE_URI).expect(OK);
         });
     });
 
     describe('POST request', () => {
         it('should return 302 and redirect to reason-other page', async () => {
-            const app = createApp(generateApplicationData());
+            const app = createApp(applicationData);
             await request(app).post(OTHER_REASON_DISCLAIMER_PAGE_URI)
                 .expect(response => {
                     expect(response.status).to.be.equal(MOVED_TEMPORARILY);
