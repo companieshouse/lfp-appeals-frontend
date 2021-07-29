8d9887b5606e7faf007da463748c602b8a257265
mouhajer-ch <70889002+mouhajer-ch@users.noreply.github.com>
2021-07-29 15:26:21 +0100

    Feature/bi 8617 update navigation on evidence question page (#309)
    * Remove unused template
    
    * Add link between illness and evidence
    
    * Add missing brackets for readability
    
    * Fix sonar issue

--- test/controllers/IllnessFurtherInformationController.test.ts
+++ test/controllers/IllnessFurtherInformationController.test.ts
@@ -5,7 +5,8 @@ import { MOVED_TEMPORARILY } from 'http-status-codes';
 import request from 'supertest';
 
 import 'app/controllers/IllnessFurtherInformationController';
-import { FURTHER_INFORMATION_PAGE_URI } from 'app/utils/Paths';
+import { Appeal } from 'app/models/Appeal';
+import { ENTRY_PAGE_URI, EVIDENCE_QUESTION_URI, FURTHER_INFORMATION_PAGE_URI } from 'app/utils/Paths';
 
 import { createApp } from 'test/ApplicationFactory';
 
@@ -17,6 +18,43 @@ describe('IllnessFurtherInformationController', () => {
             await request(app).get(FURTHER_INFORMATION_PAGE_URI).expect(response => {
                 expect(response.status).to.be.equal(MOVED_TEMPORARILY);
                 expect(response.get('Location')).to.contain('signin');
+            });
+        });
+    });
+
+    describe('POST request', () => {
+        const navigation = { permissions: [EVIDENCE_QUESTION_URI] };
+        const appeal = {
+            penaltyIdentifier: {
+                companyNumber: 'NI000000',
+                penaltyReference: 'A00000001'
+            },
+            reasons: {}
+        } as Appeal;
+
+        beforeEach(() => {
+            process.env.ILLNESS_REASON_FEATURE_ENABLED = '1';
+        });
+
+        it('should redirect to entry page when illness reason feature is disabled', async () => {
+            process.env.ILLNESS_REASON_FEATURE_ENABLED = '0';
+
+            const app = createApp({appeal});
+            await request(app).post(FURTHER_INFORMATION_PAGE_URI)
+                .send({})
+                .expect(response => {
+                    expect(response.status).to.be.equal(MOVED_TEMPORARILY);
+                    expect(response.header.location).to.include(ENTRY_PAGE_URI);
+            });
+        });
+
+        it('should redirect to Evidence Question page when posting a valid further information', async () => {
+            const app = createApp({appeal, navigation});
+            await request(app).post(FURTHER_INFORMATION_PAGE_URI)
+                .send({description: 'Something'})
+                .expect(response => {
+                    expect(response.status).to.be.equal(MOVED_TEMPORARILY);
+                    expect(response.header.location).to.include(EVIDENCE_QUESTION_URI);
             });
         });
     });
