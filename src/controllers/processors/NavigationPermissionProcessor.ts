2faaae6ed58aee1ec3b94ae3a914348166102568
mouhajer-ch <mzaouali@companieshouse.gov.uk>
2021-08-11 09:58:17 +0100

    Refactor and add tests/coverage for navigation permission processor

---
+++ src/controllers/processors/NavigationPermissionProcessor.ts
@@ -0,0 +1,31 @@
+import { Session } from 'ch-node-session-handler';
+import { provide } from 'inversify-binding-decorators';
+
+import { RequestWithNavigation } from 'app/controllers/SafeNavigationBaseController';
+import { FormActionProcessor } from 'app/controllers/processors/FormActionProcessor';
+import { loggerInstance } from 'app/middleware/Logger';
+import { ApplicationData, APPLICATION_DATA_KEY } from 'app/models/ApplicationData';
+import { EVIDENCE_UPLOAD_PAGE_URI } from 'app/utils/Paths';
+
+@provide(NavigationPermissionProcessor)
+export class NavigationPermissionProcessor implements FormActionProcessor {
+    process(request: RequestWithNavigation): void {
+        const session: Session | undefined = request.session;
+
+        if (!session) {
+            throw new Error('Session Expected but was undefined');
+        }
+
+        const applicationData: ApplicationData | undefined = session.getExtraData(APPLICATION_DATA_KEY);
+
+        if (!applicationData) {
+            const userId = session.data?.signin_info?.user_profile?.id;
+            loggerInstance()
+                .debug(`${NavigationPermissionProcessor.name} - process for userId: ${userId}`);
+
+            throw new Error('Application Data expected but was undefined');
+        }
+
+        applicationData.navigation!.permissions!.push(EVIDENCE_UPLOAD_PAGE_URI);
+    }
+}
