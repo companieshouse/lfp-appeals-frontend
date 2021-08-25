6a2216de56378c412fb878f3b031f039d530370d
dsanson <dsanson@companieshouse.gov.uk>
2021-08-25 11:39:27 +0100

    Removing redundent code from App.ts and adding service name to layout file

--- src/App.ts
+++ src/App.ts
@@ -1,27 +1,7 @@
-import express from 'express';
-import * as nunjucks from 'nunjucks';
 import './Bootstrap';
 import './LoadConfig';
 
 import { Server } from 'app/Server';
-
-const app = express();
-
-
-// view engine setup
-const env = nunjucks.configure([
-    'views',
-    'node_modules/govuk-frontend/',
-    'node_modules/govuk-frontend/components/',
-  ], {
-    autoescape: true,
-    express: app,
-  });
-  env.addGlobal('CHS_URL', process.env.CHS_URL);
-  env.addGlobal('assetPath', process.env.CDN_HOST);
-  env.addGlobal('PIWIK_URL', process.env.PIWIK_URL);
-  env.addGlobal('PIWIK_SITE_ID', process.env.PIWIK_SITE_ID);
-
 
 const server = new Server(Number(process.env.PORT) || 3000);
 server.start();
