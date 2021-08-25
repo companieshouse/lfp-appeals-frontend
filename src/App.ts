daec9cb5cb0f6febcbadf868ddb0793839440144
dsanson <dsanson@companieshouse.gov.uk>
2021-08-25 10:51:01 +0100

    Fixing layout.njk to include correct configuration to set up Matomo; removing redundent line in App.ts

--- src/App.ts
+++ src/App.ts
@@ -21,8 +21,6 @@ const env = nunjucks.configure([
   env.addGlobal('assetPath', process.env.CDN_HOST);
   env.addGlobal('PIWIK_URL', process.env.PIWIK_URL);
   env.addGlobal('PIWIK_SITE_ID', process.env.PIWIK_SITE_ID);
-//   env.addGlobal("ERROR_SUMMARY_TITLE", ErrorMessages.ERROR_SUMMARY_TITLE);
-// ^^^^ orginally a link to a .ts file containing alll enums for error msgs and codes...
 
 
 const server = new Server(Number(process.env.PORT) || 3000);
