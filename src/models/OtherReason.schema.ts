cdf9697ad638690183917ed34bc212e25fb9581f
YiannisPapastergiou <ypapastergiou@companieshouse.gov.uk>
2021-09-06 14:50:45 +0100

    Add validation to other reasons tell us why you're appealing page

--- src/models/OtherReason.schema.ts
+++ src/models/OtherReason.schema.ts
@@ -1,6 +1,7 @@
 import * as Joi from '@hapi/joi';
 
 const nameErrorMessage = 'Enter your name';
+const relationshipToCompanyErrorMessage = 'Enter your relationship to the company';
 const titleErrorMessage = 'You must give your reason a title';
 const descriptionErrorMessage = 'You must give us more information';
 
@@ -14,7 +15,15 @@ export const schema = Joi.object({
             'string.empty': nameErrorMessage,
             'string.pattern.base': nameErrorMessage
         }),
-    relationshipToCompany: Joi.string().allow('').optional(),
+    relationshipToCompany: Joi.string()
+        .required()
+        .pattern(/\w+/)
+        .messages({
+            'any.required': relationshipToCompanyErrorMessage,
+            'string.base': relationshipToCompanyErrorMessage,
+            'string.empty': relationshipToCompanyErrorMessage,
+            'string.pattern.base': relationshipToCompanyErrorMessage
+        }),
     title: Joi.string()
         .required()
         .pattern(/\w+/)
--- test/models/OtherReasonSchema.test.ts
+++ test/models/OtherReasonSchema.test.ts
@@ -7,6 +7,7 @@ import { assertValidationErrors } from 'test/models/ValidationAssertions';
 const validator = new SchemaValidator(schema);
 
 const nameErrorMessage = 'Enter your name';
+const relationshipToCompanyErrorMessage = 'Enter your relationship to the company';
 const titleErrorMessage = 'You must give your reason a title';
 const descriptionErrorMessage = 'You must give us more information';
 
@@ -15,6 +16,7 @@ describe('OtherReason schema', () => {
         const validationResult = validator.validate({});
         assertValidationErrors(validationResult, [
             new ValidationError('name', nameErrorMessage),
+            new ValidationError('relationshipToCompany', relationshipToCompanyErrorMessage),
             new ValidationError('title', titleErrorMessage),
             new ValidationError('description', descriptionErrorMessage)
         ]);
@@ -23,11 +25,13 @@ describe('OtherReason schema', () => {
     it('should reject undefined values', () => {
         const validationResult = validator.validate({
             name: undefined,
+            relationshipToCompany: undefined,
             title: undefined,
             description: undefined
         });
         assertValidationErrors(validationResult, [
             new ValidationError('name', nameErrorMessage),
+            new ValidationError('relationshipToCompany', relationshipToCompanyErrorMessage),
             new ValidationError('title', titleErrorMessage),
             new ValidationError('description', descriptionErrorMessage)
         ]);
@@ -36,11 +40,13 @@ describe('OtherReason schema', () => {
     it('should reject null values', () => {
         const validationResult = validator.validate({
             name: null,
+            relationshipToCompany: null,
             title: null,
             description: null
         });
         assertValidationErrors(validationResult, [
             new ValidationError('name', nameErrorMessage),
+            new ValidationError('relationshipToCompany', relationshipToCompanyErrorMessage),
             new ValidationError('title', titleErrorMessage),
             new ValidationError('description', descriptionErrorMessage)
         ]);
@@ -49,11 +55,13 @@ describe('OtherReason schema', () => {
     it('should reject empty values', () => {
         const validationResult = validator.validate({
             name: '',
+            relationshipToCompany: '',
             title: '',
             description: ''
         });
         assertValidationErrors(validationResult, [
             new ValidationError('name', nameErrorMessage),
+            new ValidationError('relationshipToCompany', relationshipToCompanyErrorMessage),
             new ValidationError('title', titleErrorMessage),
             new ValidationError('description', descriptionErrorMessage)
         ]);
@@ -62,11 +70,13 @@ describe('OtherReason schema', () => {
     it('should reject blank values', () => {
         const validationResult = validator.validate({
             name: ' ',
+            relationshipToCompany: ' ',
             title: ' ',
             description: ' '
         });
         assertValidationErrors(validationResult, [
             new ValidationError('name', nameErrorMessage),
+            new ValidationError('relationshipToCompany', relationshipToCompanyErrorMessage),
             new ValidationError('title', titleErrorMessage),
             new ValidationError('description', descriptionErrorMessage)
         ]);
@@ -75,6 +85,7 @@ describe('OtherReason schema', () => {
     it('should allow non empty values', () => {
         const validationResult = validator.validate({
             name: 'Some name',
+            relationshipToCompany: 'Some relationship',
             title: 'Some reason',
             description: 'Some description'
         });
@@ -85,6 +96,7 @@ describe('OtherReason schema', () => {
         const validationResult = validator.validate({
             name: ' Some name ',
             title: ' Some reason ',
+            relationshipToCompany: ' Some relationship ',
             description: ' Some description '
         });
         assertValidationErrors(validationResult, []);
