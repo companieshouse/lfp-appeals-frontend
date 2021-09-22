a10e320ac1c8c7250c96ef2294325cf97769e7df
YiannisPapastergiou <ypapastergiou@companieshouse.gov.uk>
2021-09-22 09:31:06 +0100

    Remove commented out code

--- test/controllers/IllnessEndDateController.test.ts
+++ test/controllers/IllnessEndDateController.test.ts
@@ -50,15 +50,6 @@ describe('IllnessEndDateController', () => {
 
     describe('GET request', () => {
 
-        // it('should return 500 when trying to access the page without illnessStart date set', async () => {
-        //     process.env.ILLNESS_REASON_FEATURE_ENABLED = '1';
-
-        //     const app = createApp({appeal, navigation});
-        //     await request(app).get(ILLNESS_END_DATE_PAGE_URI).expect(response => {
-        //         expect(response.status).to.be.equal(INTERNAL_SERVER_ERROR);
-        //     });
-        // });
-
         it('should return 200 when trying to access the page with illnessStart date set', async () => {
             process.env.ILLNESS_REASON_FEATURE_ENABLED = '1';
             appeal.reasons = {
