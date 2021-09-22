9faaf484f28f603302dc4e948aa10f4f7f455636
YiannisPapastergiou <68026921+YiannisPapastergiou@users.noreply.github.com>
2021-09-22 14:27:23 +0100

    Add dynamic hint text to the 'When did the illness end?' page (#358)
    * Add dynamic hint to illness end date web page
    
    * Correcting return funtionality of the GET method
    
    * Fix unit tests after the addition of the illnessStartDate in the GET method of the controller
    
    * Add a new unit test to cover the GET logical branch of illnessStart date not being set
    
    * Remove the hint text from view and add it in the controller
    
    * Remove commented out code
    
    * Remove illnessStart date from send method in 5 unit tests
    
    * Refactoring of IllnessEndDate test class
    
    * Increase GET logic branch test coverage
    
    * Remove unnecessary navigation from relevant tests

--- test/controllers/IllnessEndDateController.test.ts
+++ test/controllers/IllnessEndDateController.test.ts
@@ -22,9 +22,9 @@ import { createApp } from 'test/ApplicationFactory';
 
 const pageHeading: string = 'When did the illness end?';
 const errorSummaryHeading: string = 'There is a problem';
-const invalidStartDayErrorMessage: string = 'You must enter a day';
-const invalidStartMonthErrorMessage: string = 'You must enter a month';
-const invalidStartYearErrorMessage: string = 'You must enter a year';
+const invalidEndDayErrorMessage: string = 'You must enter a day';
+const invalidEndMonthErrorMessage: string = 'You must enter a month';
+const invalidEndYearErrorMessage: string = 'You must enter a year';
 const invalidDateErrorMessage: string = 'Enter a real date';
 const invalidDateFutureErrorMessage: string = 'Date must be today or in the past';
 const invalidEndDateBeforeStartDate: string = 'End date must be after the start date';
@@ -59,7 +59,20 @@ describe('IllnessEndDateController', () => {
 
     describe('GET request', () => {
 
-        it('should return 200 when trying to access the page', async () => {
+        it('should return 200 when trying to access the page with illnessStart date set', async () => {
+            process.env.ILLNESS_REASON_FEATURE_ENABLED = '1';
+
+            const localAppeal = { ...appeal };
+            localAppeal.reasons = { illness };
+
+            const app = createApp({appeal: localAppeal, navigation});
+            await request(app).get(ILLNESS_END_DATE_PAGE_URI).expect(response => {
+                expect(response.status).to.be.equal(OK);
+                expect(response.text).to.contain(pageHeading);
+            });
+        });
+
+        it('should return 200 when trying to access the page without illnessStart date set', async () => {
             process.env.ILLNESS_REASON_FEATURE_ENABLED = '1';
 
             const app = createApp({appeal, navigation});
@@ -69,13 +82,14 @@ describe('IllnessEndDateController', () => {
             });
         });
 
+
         it('should return 200 when trying to access the page with illness end date populated', async () => {
             process.env.ILLNESS_REASON_FEATURE_ENABLED = '1';
 
             const localAppeal = { ...appeal };
             localAppeal.reasons = { illness: { ...illness, illnessEnd: '2020-01-01' } };
 
-            const app = createApp({appeal, navigation});
+            const app = createApp({appeal: localAppeal, navigation});
             await request(app).get(ILLNESS_END_DATE_PAGE_URI).expect(response => {
                 expect(response.status).to.be.equal(OK);
                 expect(response.text).to.contain(pageHeading);
@@ -113,7 +127,6 @@ describe('IllnessEndDateController', () => {
         });
 
         it('should redirect to Futher Information page when posting a valid date', async () => {
-
             const app = createApp({appeal});
             await request(app).post(ILLNESS_END_DATE_PAGE_URI)
                 .send({day: '01', month: '01', year: '2020'})
@@ -139,28 +152,28 @@ describe('IllnessEndDateController', () => {
         it('should return 422 response with rendered error messages when empty end date was submitted', async () => {
             const app = createApp({appeal});
             await request(app).post(ILLNESS_END_DATE_PAGE_URI)
-                .send({})
+                .send()
                 .expect(response => {
                     expect(response.status).to.be.equal(UNPROCESSABLE_ENTITY);
                     expect(response.text).to.include(pageHeading)
                         .and.to.include(errorSummaryHeading)
-                        .and.to.include(invalidStartDayErrorMessage)
-                        .and.to.include(invalidStartMonthErrorMessage)
-                        .and.to.include(invalidStartYearErrorMessage)
+                        .and.to.include(invalidEndDayErrorMessage)
+                        .and.to.include(invalidEndMonthErrorMessage)
+                        .and.to.include(invalidEndYearErrorMessage)
                         .and.to.not.include(invalidDateErrorMessage)
                         .and.to.not.include(invalidDateFutureErrorMessage);
                 });
         });
 
         it('should return 422 response with rendered error messages when partial end date was submitted', async () => {
-            const app = createApp({appeal});
+            const app = createApp({appeal, navigation});
             await request(app).post(ILLNESS_END_DATE_PAGE_URI)
                 .send({month: '01', year: '2020'})
                 .expect(response => {
                     expect(response.status).to.be.equal(UNPROCESSABLE_ENTITY);
                     expect(response.text).to.include(pageHeading)
                         .and.to.include(errorSummaryHeading)
-                        .and.to.include(invalidStartDayErrorMessage)
+                        .and.to.include(invalidEndDayErrorMessage)
                         .and.to.not.include(invalidDateErrorMessage)
                         .and.to.not.include(invalidDateFutureErrorMessage);
                 });
