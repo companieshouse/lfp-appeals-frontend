86473b973947799bae4555ac58cd486ec0a9fc7e
mouhajer-ch <70889002+mouhajer-ch@users.noreply.github.com>
2021-10-19 13:45:55 +0100

    Add logic into lfp appeal landing-page to integrate gov.uk start page (#367)

--- src/controllers/LandingController.ts
+++ src/controllers/LandingController.ts
@@ -1,13 +1,18 @@
 import { controller, httpGet, httpPost, BaseHttpController } from 'inversify-express-utils';
 
-import { ENTRY_PAGE_URI, ROOT_URI } from 'app/utils/Paths';
+import { ENTRY_PAGE_URI, LANDING, ROOT_URI } from 'app/utils/Paths';
 
 @controller(ROOT_URI)
 export class LandingController extends BaseHttpController {
 
     @httpGet('')
     public renderView(): void {
-        this.httpContext.response.render('landing');
+        const { start } = this.httpContext.request.query;
+        if (start === '0') {
+            this.httpContext.response.redirect(ENTRY_PAGE_URI);
+        } else {
+            this.httpContext.response.render(LANDING);
+        }
     }
 
     @httpPost('')
